b45bd61a8f168f00f3541f8c0cda5ce6600f015c
Josh Jenkins <clearharry@gmail.com>
2023-11-19 09:05:15 +1000

    include path adjustment

--- test/test_utils.h
+++ test/test_utils.h
@@ -1,9 +1,11 @@
 #pragma once
-#include "mocks/mock_FileHandler.h"
+
+#include "MockFileHandler.h"
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <string>
 
-void expectOpenReadCloseForContent(MockFileHandler &mockFileHandler,
-                                   const std::string &content,
-                                   const std::string &filePath);
+    void
+    expectOpenReadCloseForContent(MockFileHandler &mockFileHandler,
+                                  const std::string &content,
+                                  const std::string &filePath);
