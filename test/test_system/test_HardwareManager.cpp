36ca5f482da8b0cc11c1a6e6b5fedd2726343701
Josh Jenkins <clearharry@gmail.com>
2023-11-19 08:38:20 +1000

    Initial test working

--- test/test_system/test_HardwareManager.cpp
+++ test/test_system/test_HardwareManager.cpp
@@ -1,52 +1,140 @@
+#include "ConfigManager.h"
+#include "GpioPinConfig.h"
+#include "HardwareFactory.h"
+#include "HardwareManager.h"
+#include "IFileHandler.h"
+#include "MockConfigManager.h"
+#include "MockFileHandler.h"
+#include "MockHardwareConfig.h"
+#include "MockHardwareFactory.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+/************************** Test Summary ****************************/
 /*
-To ensure the functionality and robustness of the HardwareManager, a
-comprehensive set of test cases should be devised. Here's a concise list of test
-cases to support its functionality:
 
-Initialization Tests:
+  HardwareManager Initialization Tests:
 
-Test successful initialization of all hardware components.
-Test handling of failures during hardware initialization (e.g., a component not
-responding). Configuration Loading Tests:
+  - InitializeWithValidConfig: Validates that the HardwareManager correctly
+  initializes hardware components with a valid configuration.
+  - InitializeWithInvalidConfig: Tests the HardwareManager's response to an
+  invalid or malformed hardware configuration.
+  - InitializeWithoutConfig: Checks how the HardwareManager behaves when no
+  configuration is available.
+  - SensorInitializationSuccess: Ensures proper initialization of sensors with
+  correct configuration settings.
+  - SensorInitializationFailure: Tests HardwareManager's handling of
+  unsuccessful sensor initialization.
+  - MotorInitializationSuccess: Verifies correct initialization of motors with
+  valid configuration.
+  - MotorInitializationFailure: Tests response to failed motor initialization
+  due to issues like motor driver problems.
+  - ErrorHandlingOnInitialization: Assesses the error handling during the
+  initialization process.
+  - EventNotificationPostInitialization: Checks if the HardwareManager notifies
+  other components after successful initialization.
+  - StateTransitionAfterInitialization: Evaluates state transitions using
+  TinyFSM following the initialization process.
+  - ResourceReleaseOnFailure: Tests resource cleanup and release in case of
+  initialization failure.
+  - LoggingDuringInitialization: Verifies logging of steps and issues during the
+  initialization phase.
 
-Test loading valid hardware configurations from a configuration source.
-Test response to invalid or malformed hardware configurations.
-Test handling of missing or incomplete configuration data.
-State Management Tests:
 
-Test the ability to accurately track and report the state of hardware
-components. Test state change operations (e.g., enabling/disabling components).
-Error Monitoring and Reporting Tests:
+  1. Successful Initialization
+Test Case: Ensure that HardwareManager correctly initializes hardware components
+when ConfigManager provides valid HardwareConfig data. Expectation: The hardware
+components are initialized as expected, and any relevant state in
+HardwareManager is set correctly.
+2. Handling of Missing or Invalid Configurations
+Test Case: Simulate a scenario where ConfigManager returns nullptr (indicating
+missing configuration) and verify how HardwareManager handles it. Expectation:
+HardwareManager should not attempt to initialize hardware components and should
+handle the situation gracefully, possibly setting an error state or logging an
+appropriate message.
+3. Error Conditions and Robustness
+Test Case: Introduce mock errors or exceptions in the configuration loading
+process and observe how HardwareManager responds. Expectation: The manager
+should handle errors robustly, without crashing or entering an invalid state.
+4. Correct Interaction with ConfigManager
+Test Case: Verify that HardwareManager calls the correct methods on
+ConfigManager to retrieve the necessary configurations. Expectation: Methods
+such as getHardwareConfig() are called as expected.
+5. Handling Partial or Incomplete Configurations
+Test Case: Provide a HardwareConfig with partial or incomplete data (e.g.,
+missing some GPIO configurations) and test how HardwareManager responds.
+Expectation: HardwareManager should either handle the incomplete configuration
+gracefully or log appropriate errors/warnings.
+6. State Verification Post Initialization
+Test Case: After successful initialization, verify that the state of
+HardwareManager reflects the initialized hardware. Expectation: Internal state
+variables or flags in HardwareManager should correctly represent the
+initialization status.
+7. Multiple Initializations
+Test Case: Call the initialization method on HardwareManager multiple times,
+possibly with different configurations. Expectation: Subsequent initializations
+either update the hardware state correctly or are handled appropriately (e.g.,
+ignored, logged, etc.).
 
-Test detection and reporting of hardware faults or anomalies.
-Test the system's response to simulated hardware failures.
-Resource Allocation Tests:
-
-Test successful allocation and release of hardware resources.
-Test handling of resource conflicts or shortages.
-Performance Monitoring Tests:
-
-Test monitoring of hardware performance metrics.
-Test adaptive adjustments based on performance data (if applicable).
-Interface Abstraction Tests:
-
-Test the uniformity and reliability of the interface for different hardware
-components. Test the interface's handling of various hardware operations. Safe
-Operation and Shutdown Tests:
-
-Test the system's ability to operate within safe hardware parameters.
-Test proper shutdown procedures and transitions to low-power states.
-Robustness Tests:
-
-Test resilience to unexpected hardware behaviors or inputs.
-Test recovery mechanisms from hardware-related errors.
-Scalability Tests:
-
-Test the addition of new hardware components to the system.
-Test system behavior under scaled-up hardware configurations.
-Integration Tests:
-
-Test interaction with other system components like ConfigManager and
-SystemController. Test end-to-end functionality in a simulated or real
-environment.
 */
+/*******************************************************************/
+
+using ::testing::_;
+using ::testing::DoAll;
+using ::testing::Invoke;
+using ::testing::Return;
+using ::testing::ReturnRef;
+
+class HardwareManagerTest : public ::testing::Test {
+protected:
+  std::shared_ptr<MockFileHandler> mockFileHandler;
+  std::shared_ptr<MockConfigManager> mockConfigManager;
+  std::shared_ptr<MockHardwareConfig> mockHardwareConfig;
+  std::unique_ptr<MockHardwareFactory> mockHardwareFactory;
+
+  HardwareManager hardwareManager;
+
+  HardwareManagerTest()
+      : mockFileHandler(new MockFileHandler()),
+        mockConfigManager(new MockConfigManager(mockFileHandler.get())),
+        mockHardwareConfig(new MockHardwareConfig(mockFileHandler.get())),
+        mockHardwareFactory(new MockHardwareFactory()),
+        hardwareManager(mockConfigManager, std::move(mockHardwareFactory)) {}
+
+  virtual void SetUp() override {
+    // Setup code (if needed)
+  }
+};
+
+TEST_F(HardwareManagerTest, InitializeWithValidConfig) {
+  // Set up mock config and expectations
+  GpioPinConfig pin1{1, "Pin1", "DigitalIO"};
+  GpioPinConfig pin2{2, "Pin2", "PWM"};
+  GpioPinConfig pin3{3, "Pin3", "DigitalIO"};
+  pin3.options["mode"] = "INPUT";
+
+  std::vector<GpioPinConfig> gpioConfigurations = {pin1, pin2, pin3};
+
+  EXPECT_CALL(*mockConfigManager, getHardwareConfig())
+      .WillOnce(Return(mockHardwareConfig));
+
+  EXPECT_CALL(*mockHardwareConfig, getGpioConfigs())
+      .WillOnce(ReturnRef(gpioConfigurations));
+
+  // Call initializeHardware()
+  hardwareManager.initializeHardware();
+
+  // Verify components initialized
+  EXPECT_TRUE(hardwareManager.isComponentInitialized("DigitalIO"));
+  EXPECT_TRUE(hardwareManager.isComponentInitialized("PWM"));
+}
+
+TEST_F(HardwareManagerTest, InitializeWithoutConfig) {
+  EXPECT_CALL(*mockConfigManager, getHardwareConfig())
+      .WillOnce(Return(nullptr));
+
+  hardwareManager.initializeHardware();
+
+  EXPECT_FALSE(hardwareManager.isComponentInitialized("DigitalIO"));
+  EXPECT_FALSE(hardwareManager.isComponentInitialized("PWM"));
+}
