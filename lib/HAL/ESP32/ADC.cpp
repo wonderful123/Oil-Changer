6e064650b3cdc7a37d6c28df755c500b74da666b
Josh Jenkins <clearharry@gmail.com>
2023-11-19 15:20:45 +1000

    fixed constructor initializer

--- lib/HAL/ESP32/ADC.cpp
+++ lib/HAL/ESP32/ADC.cpp
@@ -1,10 +1,11 @@
 #include "ADC.h"
 #ifdef PLATFORM_ESP32
-#include <Arduino.h>
 #include "Logger.h"
+#include <Arduino.h>
+#include <driver/adc.h>
 
 ADC::ADC(const GpioPinConfig &config)
-    : IADC(config), _resolution(12), _attenuation(ADC_0db) {
+    : IADC(config), _attenuation(ADC_0db) {
   // Set default values for resolution and attenuation
   // Default resolution for ESP32 is usually 12 bits
   // Default attenuation is 0 dB
@@ -24,12 +25,12 @@ ADC::ADC(const GpioPinConfig &config)
     if (_attenuation >= 0 && _attenuation <= 3) {
       // Configure ADC attenuation
       adc1_config_channel_atten(static_cast<adc1_channel_t>(_pinNumber),
-                                static_cast<adc_attenuation_t>(_attenuation));
+                                static_cast<adc_atten_t>(_attenuation));
     } else {
       // Handle invalid attenuation values
       Logger::error("Invalid ADC attenuation value.");
       setInitialized(false);
-      
+
       return;
     }
   }
