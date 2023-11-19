1deb6c2c83e8903cb696267109ec691337f4a09c
Josh Jenkins <clearharry@gmail.com>
2023-11-20 09:13:32 +1000

    implemented extended getOptionAs function

--- lib/Config/GpioPinConfig.h
+++ lib/Config/GpioPinConfig.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <sstream>
 #include <string>
 #include <unordered_map>
 
@@ -12,10 +13,18 @@ struct GpioPinConfig {
   GpioPinConfig(int pin, const std::string &id, const std::string &type)
       : pinNumber(pin), id(id), type(type) {}
 
-  // Optional: Method to retrieve option with type casting
-  template <typename T> T getOptionAs(const std::string &key) const {
-    // Implement logic to convert the string to the desired type (T) and return
-    // it Handle cases where the key is not found or the conversion is not
-    // possible
+  template <typename T>
+  T getOptionAs(const std::string &key) const {
+    auto it = options.find(key);
+    if (it == options.end()) {
+      return -1; // Return default value if key not found
+    }
+
+    std::istringstream iss(it->second);
+    T value;
+    if (!(iss >> value) || iss.fail()) {
+      return -1; // Return default value if conversion fails
+    }
+    return value;
   }
 };
