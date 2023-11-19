0632bcb6dfd51fc3e371371076f2a32ad6e630f0
Josh Jenkins <clearharry@gmail.com>
2023-11-19 15:21:55 +1000

    fixed mode type casting from string to enum

--- lib/HAL/ESP32/DigitalIO.cpp
+++ lib/HAL/ESP32/DigitalIO.cpp
@@ -1,10 +1,23 @@
 #include "DigitalIO.h"
 #ifdef PLATFORM_ESP32
 #include <Arduino.h>
+#include "Logger.h"
 
 DigitalIO::DigitalIO(const GpioPinConfig &config) : IDigitalIO(config) {
-  pinMode(_pinNumber, static_cast<uint8_t>(config.mode));
-  setInitialized(true); // Mark as initialized
+  std::string modeStr = config.options.at("mode");
+  if (modeStr == "INPUT") {
+    _mode = IDigitalIO::INPUT_MODE;
+  } else if (modeStr == "OUTPUT") {
+    _mode = IDigitalIO::OUTPUT_MODE;
+  } else {
+    Logger::error("Invalid mode in DigitalIO configuration.");
+    setInitialized(false);
+    return;
+  }
+
+  pinMode(_pinNumber,
+          _mode == IDigitalIO::INPUT_MODE ? INPUT_MODE : OUTPUT_MODE);
+  setInitialized(true);
 }
 
 int DigitalIO::read() const { return digitalRead(_pinNumber); }
