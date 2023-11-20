a923d3add824e15ac73b514bbaf0fb6b406cba83
Josh Jenkins <clearharry@gmail.com>
2023-11-20 10:37:31 +1000

    handle default values for string

--- lib/Config/GpioPinConfig.h
+++ lib/Config/GpioPinConfig.h
@@ -2,29 +2,43 @@
 
 #include <sstream>
 #include <string>
+#include <type_traits>
 #include <unordered_map>
 
 struct GpioPinConfig {
   int pinNumber;
   std::string id;
   std::string type;
-  std::unordered_map<std::string, std::string> options; // Flexible options map
+  std::unordered_map<std::string, std::string> options;
 
   GpioPinConfig(int pin, const std::string &id, const std::string &type)
       : pinNumber(pin), id(id), type(type) {}
 
-  template <typename T>
-  T getOptionAs(const std::string &key) const {
+  template <typename T> T getOptionAs(const std::string &key) const {
     auto it = options.find(key);
     if (it == options.end()) {
-      return -1; // Return default value if key not found
+      return defaultValue<T>();
     }
 
     std::istringstream iss(it->second);
     T value;
     if (!(iss >> value) || iss.fail()) {
-      return -1; // Return default value if conversion fails
+      return defaultValue<T>();
     }
     return value;
+  }
+
+private:
+  template <typename T>
+  static typename std::enable_if<!std::is_same<T, std::string>::value, T>::type
+  defaultValue() {
+    return T(); // Return default-constructed value for non-string types
+  }
+
+  // Specialization for std::string
+  template <typename T>
+  static typename std::enable_if<std::is_same<T, std::string>::value, T>::type
+  defaultValue() {
+    return ""; // Return empty string for std::string
   }
 };
