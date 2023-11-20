e774df4e1a691513fcf1ef3fd6c309525af03f1e
Josh Jenkins <clearharry@gmail.com>
2023-11-20 11:30:09 +1000

    simplified mock

--- lib/Mocks/MockDigitalIO.h
+++ lib/Mocks/MockDigitalIO.h
@@ -1,65 +1,10 @@
-#include "IDigitalIO.h"
-#include <iostream>
-#include <string>
+#include "DigitalIOBase.h"
+#include "gmock/gmock.h"
 
-#ifdef PLATFORM_NATIVE
-#ifndef LOW
-#define LOW 0
-#endif
-
-#ifndef HIGH
-#define HIGH 1
-#endif
-
-class MockDigitalIO : public IDigitalIO {
+class MockDigitalIO : public DigitalIOBase {
 public:
-  explicit MockDigitalIO(const GpioPinConfig &config)
-      : IDigitalIO(config), _mockValue(LOW) {
-    std::string modeStr = "OUTPUT"; // Default mode
-    auto it = config.options.find("mode");
-    if (it != config.options.end()) {
-      modeStr = it->second;
-    }
-    
-    _mode == IDigitalIO::INPUT_MODE ? IDigitalIO::INPUT_MODE : IDigitalIO::OUTPUT_MODE;
+  explicit MockDigitalIO(const GpioPinConfig &config) : DigitalIOBase(config) {}
 
-    std::cout << "MockDigitalIO initialized on pin: " << _pinNumber
-              << " with mode: "
-              << (_mode == IDigitalIO::INPUT_MODE ? "INPUT" : "OUTPUT")
-              << std::endl;
-    setInitialized(true); // Mark MockDigitalIO as initialized
-  }
-
-  virtual int read() const override {
-    if (!isInitialized()) {
-      std::cerr << "MockDigitalIO: Attempted to read uninitialized DigitalIO."
-                << std::endl;
-      return -1; // or some error code
-    }
-    // Return a simulated digital IO value
-    return _mockValue;
-  }
-
-  virtual void write(int value) override {
-    if (!isInitialized()) {
-      std::cerr
-          << "MockDigitalIO: Attempted to write to uninitialized DigitalIO."
-          << std::endl;
-      return;
-    }
-    _mockValue = value; // Simulate writing a value
-    std::cout << "MockDigitalIO: Pin " << _pinNumber << " set to "
-              << (_mockValue == HIGH ? "HIGH" : "LOW") << std::endl;
-  }
-
-  virtual Mode getMode() const override { return _mode; }
-
-  // Additional methods for testing
-  void setMockValue(int value) { _mockValue = value; }
-
-private:
-  int _mockValue; // Simulated Digital IO value
-  Mode _mode;     // Mode of the Digital IO
+  MOCK_METHOD(int, read, (), (const, override));
+  MOCK_METHOD(void, write, (int value), (override));
 };
-
-#endif // PLATFORM_NATIVE
