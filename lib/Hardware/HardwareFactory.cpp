16edeea3a8640f2f83b470e7673a18ef7d88ccad
Josh Jenkins <clearharry@gmail.com>
2023-11-22 15:34:35 +1000

    added function to create component based on config

--- lib/Hardware/HardwareFactory.cpp
+++ lib/Hardware/HardwareFactory.cpp
@@ -5,16 +5,42 @@
 #include <MockHardwareFactory.h>
 #endif
 
-std::unique_ptr<HardwareFactory> &getHardwareFactory() {
-  static std::unique_ptr<HardwareFactory> hardwareFactory;
+std::unique_ptr<HardwareComponent>
+HardwareFactory::createComponent(const GpioPinConfig &config) {
+  std::unique_ptr<HardwareComponent> hardwareComponent;
 
-  if (!hardwareFactory) {
-#ifdef PLATFORM_ESP32
-    hardwareFactory.reset(new ESP32HardwareFactory());
-#else
-    hardwareFactory.reset(new MockHardwareFactory());
-#endif
+  if (config.type == "ADC") {
+    hardwareComponent = createADC(config);
+  } else if (config.type == "Button") {
+    hardwareComponent = createButton(config);
+  } else if (config.type == "DAC") {
+    hardwareComponent = createDAC(config);
+  } else if (config.type == "DigitalIO") {
+    hardwareComponent = createDigitalIO(config);
+  } else if (config.type == "PWM") {
+    hardwareComponent = createPWM(config);
+  } else if (config.type == "Buzzer") {
+    hardwareComponent = createBuzzer(config);
+  } else {
+    Logger::error("Unknown hardware component type in hardware factory: " + config.type);
+    return nullptr;
   }
 
+  if (!hardwareComponent) {
+    Logger::error("Failed to create hardware component in hardware factory: " + config.type);
+  }
+
+  return hardwareComponent;
+}
+
+std::unique_ptr<HardwareFactory> &HardwareFactory::getHardwareFactory() {
+  static std::unique_ptr<HardwareFactory> hardwareFactory;
+#ifdef PLATFORM_ESP32
+  if (!hardwareFactory)
+    hardwareFactory.reset(new ESP32HardwareFactory());
+#else
+  if (!hardwareFactory)
+    hardwareFactory.reset(new MockHardwareFactory());
+#endif
   return hardwareFactory;
 }
--- lib/Hardware/HardwareFactory.h
+++ lib/Hardware/HardwareFactory.h
@@ -1,22 +1,26 @@
 #pragma once
 
 #include "GpioPinConfig.h"
+#include "HardwareComponent.h"
 #include "IADC.h"
 #include "IButton.h"
 #include "IBuzzer.h"
 #include "IDAC.h"
 #include "IDigitalIO.h"
-#include "IFileHandler.h"
 #include "IPWM.h"
+#include "IFileHandler.h"
 #include <memory>
 
 class HardwareFactory {
 public:
   virtual ~HardwareFactory() = default;
 
-  // Factory getter function declaration
-  std::unique_ptr<HardwareFactory> &getHardwareFactory();
+  // Singleton getter function
+  static std::unique_ptr<HardwareFactory> &getHardwareFactory();
 
+  std::unique_ptr<HardwareComponent> createComponent(const GpioPinConfig &config);
+
+  // Factory methods for creating hardware components
   virtual std::unique_ptr<IADC> createADC(const GpioPinConfig &config) = 0;
   virtual std::unique_ptr<IButton> createButton(const GpioPinConfig &config) = 0;
   virtual std::unique_ptr<IDAC> createDAC(const GpioPinConfig &config) = 0;
@@ -24,4 +28,7 @@ public:
   virtual std::unique_ptr<IPWM> createPWM(const GpioPinConfig &config) = 0;
   virtual std::unique_ptr<IBuzzer> createBuzzer(const GpioPinConfig &config) = 0;
   virtual std::unique_ptr<IFileHandler> createFileHandler() = 0;
+
+private:
+  std::unique_ptr<HardwareFactory> _hardwareFactory;
 };
