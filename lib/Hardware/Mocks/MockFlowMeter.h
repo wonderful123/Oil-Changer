841e0ba3d4706205de00fca77d860728673e7063
Josh Jenkins <clearharry@gmail.com>
2023-11-25 09:34:04 +1000

    change inheritance to base

--- lib/Hardware/Mocks/MockFlowMeter.h
+++ lib/Hardware/Mocks/MockFlowMeter.h
@@ -1,18 +1,18 @@
 #pragma once
 
-#include "IFlowMeter.h"
-
 #ifdef PLATFORM_NATIVE
 
+#include "FlowMeterBase.h"
 #include <gmock/gmock.h>
 
-class MockFlowMeter : public IFlowMeter {
+class MockFlowMeter : public FlowMeterBase {
 public:
-  explicit MockFlowMeter(const HardwarePinConfig &config);
+  explicit MockFlowMeter(const HardwarePinConfig &config)
+      : FlowMeterBase(config) {}
 
-  MOCK_METHOD(double, getFlowRate, (), (const, override));
-  MOCK_METHOD(double, getTotalVolume, (), (const, override));
-  MOCK_METHOD(void, reset, (), (override));
-};
+    MOCK_METHOD(double, getFlowRate, (), (const, override));
+    MOCK_METHOD(double, getTotalVolume, (), (const, override));
+    MOCK_METHOD(void, reset, (), (override));
+  };
 
 #endif // PLATFORM_NATIVE
