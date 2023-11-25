ecefed33d5f44c74ac35f5c0829ad771fb4eef25
Josh Jenkins <clearharry@gmail.com>
2023-11-25 14:45:35 +1000

    handle bools

--- lib/Config/HardwarePinConfig.h
+++ lib/Config/HardwarePinConfig.h
@@ -12,32 +12,18 @@ struct HardwarePinConfig {
   std::unordered_map<std::string, int> pins;
   std::unordered_map<std::string, std::string> options; // Additional options
 
-  // Constructor for single-pin configuration
+  // Constructors
   HardwarePinConfig(
-      int pinNumber, 
-      const std::string &id, 
-      const std::string &type,
+      int pinNumber, const std::string &id, const std::string &type,
       const std::unordered_map<std::string, std::string> &options = {})
       : id(id), type(type), pinNumber(pinNumber), options(options) {}
 
-  // Constructor for multi-pin configuration
   HardwarePinConfig(
-      const std::unordered_map<std::string, int> &pins,
-      const std::string &id, 
+      const std::unordered_map<std::string, int> &pins, const std::string &id,
       const std::string &type,
       const std::unordered_map<std::string, std::string> &options = {})
       : id(id), type(type), pinNumber(-1), pins(pins), options(options) {}
 
-  template <typename T> T getOptionAs(const std::string &key) const {
-    auto it = options.find(key);
-    if (it != options.end()) {
-      std::stringstream ss(it->second);
-      T value;
-      if (ss >> value) {
-        return value;
-      }
-    }
-    return T(); // Return default value if option is not found or conversion
-                // fails
-  }
+  // Template method for getting options
+  template <typename T> T getOptionAs(const std::string &key) const;
 };
