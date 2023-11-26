9120edd199d4668af131a22682ef2a7b58ea64ef
Josh Jenkins <clearharry@gmail.com>
2023-11-26 20:36:45 +1000

    improved mock

--- lib/Hardware/MockHardwareFactory.h
+++ lib/Hardware/MockHardwareFactory.h
@@ -4,64 +4,69 @@
 
 #include "HardwareFactory.h"
 #include "Mocks/MockADC.h"
-#include "Mocks/MockDAC.h"
+#include "Mocks/MockButton.h"
 #include "Mocks/MockBuzzer.h"
+#include "Mocks/MockDAC.h"
 #include "Mocks/MockDigitalIO.h"
 #include "Mocks/MockFileHandler.h"
-#include "Mocks/MockPWM.h"
-#include "Mocks/MockButton.h"
 #include "Mocks/MockFlowMeter.h"
 #include "Mocks/MockI2C.h"
+#include "Mocks/MockPWM.h"
 #include "Mocks/MockSPI.h"
 #include "Mocks/MockSerial.h"
+#include <gmock/gmock.h>
 
 class MockHardwareFactory : public HardwareFactory {
 public:
-  MOCK_METHOD(std::unique_ptr<HardwareComponent>, createComponent,
+  // Mock methods for each type of component creation
+  MOCK_METHOD(std::unique_ptr<IADC>, createADC, (const HardwarePinConfig &));
+  MOCK_METHOD(std::unique_ptr<IButton>, createButton,
+              (const HardwarePinConfig &));
+  MOCK_METHOD(std::unique_ptr<IDAC>, createDAC, (const HardwarePinConfig &));
+  MOCK_METHOD(std::unique_ptr<IDigitalIO>, createDigitalIO,
+              (const HardwarePinConfig &));
+  MOCK_METHOD(std::unique_ptr<IFlowMeter>, createFlowMeter,
+              (const HardwarePinConfig &));
+  MOCK_METHOD(std::unique_ptr<IPWM>, createPWM, (const HardwarePinConfig &));
+  MOCK_METHOD(std::unique_ptr<IBuzzer>, createBuzzer,
+              (const HardwarePinConfig &));
+  MOCK_METHOD(std::unique_ptr<ISPI>, createSPI, (const HardwarePinConfig &));
+  MOCK_METHOD(std::unique_ptr<II2C>, createI2C, (const HardwarePinConfig &));
+  MOCK_METHOD(std::unique_ptr<ISerial>, createSerial,
               (const HardwarePinConfig &));
 
-  std::unique_ptr<IADC> createADC(const HardwarePinConfig &config) override {
-    return std::unique_ptr<IADC>(new MockADC(config));
-  }
-
-  std::unique_ptr<IButton> createButton(const HardwarePinConfig &config) override {
-    return std::unique_ptr<IButton>(new MockButton(config));
-  }
-
-  std::unique_ptr<IDAC> createDAC(const HardwarePinConfig &config) override {
-    return std::unique_ptr<IDAC>(new MockDAC(config));
-  }
-
-  std::unique_ptr<IFlowMeter> createFlowMeter(const HardwarePinConfig &config) override {
-    return std::unique_ptr<IFlowMeter>(new MockFlowMeter(config));
-  }
-
-  std::unique_ptr<IDigitalIO> createDigitalIO(const HardwarePinConfig &config) override {
-    return std::unique_ptr<IDigitalIO>(new MockDigitalIO(config));
-  }
-
-  std::unique_ptr<IPWM> createPWM(const HardwarePinConfig &config) override {
-    return std::unique_ptr<IPWM>(new MockPWM(config));
-  }
-
-  std::unique_ptr<IBuzzer> createBuzzer(const HardwarePinConfig &config) override {
-    return std::unique_ptr<IBuzzer>(new MockBuzzer(config));
-  }
-
-  std::unique_ptr<IFileHandler> createFileHandler() override {
+  std::unique_ptr<IFileHandler> createFileHandler() {
     return std::unique_ptr<IFileHandler>(new MockFileHandler());
   }
 
-  std::unique_ptr<II2C> createI2C(const HardwarePinConfig &config) override {
-    return std::unique_ptr<II2C>(new MockI2C(config));
-  }
-
-  std::unique_ptr<ISPI> createSPI(const HardwarePinConfig &config) override {
-    return std::unique_ptr<ISPI>(new MockSPI(config));
-  }
-
-  std::unique_ptr<ISerial> createSerial(const HardwarePinConfig &config) override {
-    return std::unique_ptr<ISerial>(new MockSerial(config));
+  std::unique_ptr<HardwareComponent>
+  createComponent(const HardwarePinConfig &config) override {
+    if (config.type == "ADC") {
+      return createADC(config);
+    } else if (config.type == "Button") {
+      return createButton(config);
+    } else if (config.type == "DAC") {
+      return createDAC(config);
+    } else if (config.type == "DigitalIO") {
+      return createDigitalIO(config);
+    } else if (config.type == "FlowMeter") {
+      return createFlowMeter(config);
+    } else if (config.type == "PWM") {
+      return createPWM(config);
+    } else if (config.type == "Buzzer") {
+      return createBuzzer(config);
+    } else if (config.type == "SPI") {
+      return createSPI(config);
+    } else if (config.type == "I2C") {
+      return createI2C(config);
+    } else if (config.type == "Serial") {
+      return createSerial(config);
+    } else {
+      Logger::error(
+          "Unknown hardware component type in mock hardware factory: " +
+          config.type);
+      return nullptr;
+    }
   }
 };
 
