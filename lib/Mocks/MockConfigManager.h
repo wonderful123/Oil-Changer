d811732fb8a8953809fd2d22d6fc36457ab75c61
Josh Jenkins <clearharry@gmail.com>
2023-11-26 14:17:16 +1000

    mock the rest of class

--- lib/Mocks/MockConfigManager.h
+++ lib/Mocks/MockConfigManager.h
@@ -3,6 +3,8 @@
 #ifdef PLATFORM_NATIVE
 
 #include "ConfigManager.h"
+#include "Error.h"
+#include "HardwareConfig.h"
 #include <gmock/gmock.h>
 #include <memory>
 
@@ -11,8 +13,12 @@ public:
   explicit MockConfigManager(IFileHandler *fileHandler)
       : ConfigManager(fileHandler) {}
 
-  MOCK_METHOD(std::shared_ptr<HardwareConfig>, getHardwareConfig, (),
+  // Mock the getHardwareConfig method
+  MOCK_METHOD((std::shared_ptr<HardwareConfig>), getHardwareConfig, (),
               (const, override));
+
+  // Mock the loadConfig method
+  MOCK_METHOD(Error, loadConfig, (const std::string &));
 };
 
 #endif // PLATFORM_NATIVE
