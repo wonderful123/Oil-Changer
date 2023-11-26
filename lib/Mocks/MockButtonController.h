c6a27c3890f11e8d934d161fb17e7245cbd54842
Josh Jenkins <clearharry@gmail.com>
2023-11-27 08:42:33 +1000

    completed mock

--- lib/Mocks/MockButtonController.h
+++ lib/Mocks/MockButtonController.h
@@ -15,8 +15,10 @@ public:
 
 class MockButtonController : public ButtonController {
 public:
-  MOCK_METHOD(void, registerButton,
-              (const std::string &, std::shared_ptr<IButton>), (override));
+  MOCK_METHOD(void, addObserver, (std::shared_ptr<IButtonControllerObserver> observer), (override));
+  MOCK_METHOD(void, registerButton, (const std::string &id, std::shared_ptr<IButton> button), (override));
+  MOCK_METHOD(void, notifyObservers, (const std::string &id), (override));
+  MOCK_METHOD(void, checkButtonStates, (), (override));
 };
 
 #endif // PLATFORM_NATIVE
