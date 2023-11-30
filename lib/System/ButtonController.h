934264d5476d72520cc2f2b4ebd81e7fc5c3f77a
Josh Jenkins <clearharry@gmail.com>
2023-11-30 15:44:07 +1000

    moved notifyObservers back to public

--- lib/System/ButtonController.h
+++ lib/System/ButtonController.h
@@ -8,13 +8,13 @@
 
 class ButtonController {
 public:
-  void addObserver(std::shared_ptr<IButtonControllerObserver> observer);
-  void registerButton(const std::string &id, std::shared_ptr<IButton> button);
-  void processButtonStates();
-  std::shared_ptr<IButton> getButtonById(const std::string &id) const;
+  virtual void addObserver(std::shared_ptr<IButtonControllerObserver> observer);
+  virtual void registerButton(const std::string &id, std::shared_ptr<IButton> button);
+  virtual void processButtonStates();
+  virtual std::shared_ptr<IButton> getButtonById(const std::string &id) const;
+  virtual void notifyObservers(const std::string &id);
 
 private:
-  void notifyObservers(const std::string &id);
   std::vector<std::shared_ptr<IButtonControllerObserver>> _observers;
   std::unordered_map<std::string, std::shared_ptr<IButton>> _buttons;
 };
