3d7e413e15344fb7c83e890eac0e76a5010f9c51
Josh Jenkins <clearharry@gmail.com>
2023-12-01 10:51:43 +1000

    implemented parsing

--- lib/Config/InteractionSettingsConfig.cpp
+++ lib/Config/InteractionSettingsConfig.cpp
@@ -1,9 +1,110 @@
 #include "InteractionSettingsConfig.h"
 
 Error InteractionSettingsConfig::save(const std::string &filename) const {
-  return Error();
+  // TODO: Implement this function
+  return Error(Error::OK);
 }
 
 Error InteractionSettingsConfig::parseJson(const DynamicJsonDocument &doc) {
-  return Error();
+  if (!doc.containsKey("buttonInteraction")) {
+    return Error(Error::InteractionSettingsButtonInteractionKeyMissing);
+  }
+
+  const JsonObjectConst buttonInteraction = doc["buttonInteraction"];
+
+  if (!buttonInteraction.containsKey("buttons")) {
+    return Error(Error::InteractionSettingsButtonsSubkeyMissing);
+  } else {
+    Error err =
+        parseButtonSettings(buttonInteraction["buttons"].as<JsonObjectConst>());
+    if (err)
+      return err;
+  }
+
+  if (!buttonInteraction.containsKey("beepSettings")) {
+    return Error(Error::InteractionSettingsBeepSettingsSubkeyMissing);
+  } else {
+    Error err = parseBeepSettings(
+        buttonInteraction["beepSettings"].as<JsonObjectConst>());
+    if (err)
+      return err;
+  }
+
+  if (!buttonInteraction.containsKey("feedback")) {
+    return Error(Error::InteractionSettingsFeedbackSubkeyMissing);
+  } else {
+    Error err = parseFeedbackSettings(
+        buttonInteraction["feedback"].as<JsonObjectConst>());
+    if (err)
+      return err;
+  }
+
+  return Error(Error::OK);
+}
+
+Error InteractionSettingsConfig::parseButtonSettings(
+    const JsonObjectConst &buttonObj) {
+  for (auto buttonPair : buttonObj) {
+    const std::string buttonKey = buttonPair.key().c_str();
+    const JsonObjectConst buttonSettingsObj =
+        buttonPair.value().as<JsonObjectConst>();
+
+    InteractionSettings::Button button;
+    button.description = buttonSettingsObj["description"].as<std::string>();
+
+    // Parsing 'autoRepeat' settings
+    const JsonObjectConst autoRepeatObj =
+        buttonSettingsObj["autoRepeat"].as<JsonObjectConst>();
+    InteractionSettings::AutoRepeat autoRepeat;
+    autoRepeat.initialDelayMs = autoRepeatObj["initialDelayMs"];
+    autoRepeat.standardRateMs = autoRepeatObj["standardRateMs"];
+
+    const JsonObjectConst accelerationObj =
+        autoRepeatObj["acceleration"].as<JsonObjectConst>();
+    autoRepeat.acceleration.startAfterMs = accelerationObj["startAfterMs"];
+    autoRepeat.acceleration.rateDecreaseIntervalMs =
+        accelerationObj["rateDecreaseIntervalMs"];
+    autoRepeat.acceleration.minimumRateMs = accelerationObj["minimumRateMs"];
+
+    button.autoRepeat = autoRepeat;
+    _interactionSettings.buttons[buttonKey] = button;
+  }
+
+  return Error(Error::OK);
+}
+
+Error InteractionSettingsConfig::parseBeepSettings(
+    const JsonObjectConst &beepSettings) {
+  InteractionSettings::BeepSettings settings;
+  settings.description = beepSettings["description"].as<std::string>();
+  settings.standardFrequency = beepSettings["standardFrequency"];
+  settings.standardDurationMs = beepSettings["standardDurationMs"];
+
+  const JsonObjectConst limitPattern = beepSettings["limitReachedPattern"];
+  settings.limitReachedPattern.description =
+      limitPattern["description"].as<std::string>();
+  settings.limitReachedPattern.frequency = limitPattern["frequency"];
+  settings.limitReachedPattern.durationMs = limitPattern["durationMs"];
+  settings.limitReachedPattern.pattern =
+      limitPattern["pattern"].as<std::string>();
+
+  _interactionSettings.beepSettings = settings;
+
+  return Error(Error::OK);
+}
+
+Error InteractionSettingsConfig::parseFeedbackSettings(
+    const JsonObjectConst &feedbackSettings) {
+  InteractionSettings::Feedback feedback;
+  feedback.description = feedbackSettings["description"].as<std::string>();
+  feedback.onButtonRelease = feedbackSettings["onButtonRelease"];
+  feedback.audioFeedbackOnLimit = feedbackSettings["audioFeedbackOnLimit"];
+
+  _interactionSettings.feedback = feedback;
+
+  return Error(Error::OK);
+}
+
+InteractionSettings InteractionSettingsConfig::getSettings() {
+  return _interactionSettings;
 }
--- lib/Config/InteractionSettingsConfig.h
+++ lib/Config/InteractionSettingsConfig.h
@@ -1,16 +1,26 @@
+#pragma once
+
 #include "BaseConfig.h"
 #include "Error.h"
+#include "InteractionSettings.h"
 
 class InteractionSettingsConfig : public BaseConfig {
 public:
-  explicit InteractionSettingsConfig(IFileHandler *fileHandler)
-      : BaseConfig(fileHandler) {}
+  using BaseConfig::BaseConfig;
 
   Error save(const std::string &filename) const override;
+
+  InteractionSettings getSettings();
 
 protected:
   Error parseJson(const DynamicJsonDocument &doc) override;
 
-private:
+  Error parseButtonSettings(const JsonObjectConst &buttonObj);
 
+  Error parseBeepSettings(const JsonObjectConst &beepSettings);
+
+  Error parseFeedbackSettings(const JsonObjectConst &feedbackSettings);
+
+private:
+  InteractionSettings _interactionSettings;
 };
