f3ac1d7d3ad15a05542916cd88a8b1e0f38f9045
Josh Jenkins <clearharry@gmail.com>
2023-12-01 10:52:53 +1000

    added auto-repeat settings

--- lib/HAL/IButton.h
+++ lib/HAL/IButton.h
@@ -2,7 +2,9 @@
 
 #include "HardwareComponent.h"
 #include "HardwarePinConfig.h"
+#include "InteractionSettings.h"
 #include <functional>
+#include <string>
 
 class IButton : public HardwareComponent {
 public:
@@ -12,8 +14,12 @@ public:
 
   virtual bool isPressed() const = 0; // Check if the button is pressed
 
-  using ButtonPressCallback = void (*)(const std::string &);
+  using ButtonPressCallback = std::function<void(const std::string &)>;
   virtual void setOnPressCallback(ButtonPressCallback callback) = 0;
 
   virtual void update() = 0; // Update the button's state
+
+  // Add method to set auto-repeat settings
+  virtual void
+  setAutoRepeatSettings(const InteractionSettings::AutoRepeat &settings) = 0;
 };
