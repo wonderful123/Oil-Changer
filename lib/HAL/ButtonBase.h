be080f20d6d91423e3d3fb567546666799033964
Josh Jenkins <clearharry@gmail.com>
2023-12-01 10:52:37 +1000

    added auto-repeat settings

--- lib/HAL/ButtonBase.h
+++ lib/HAL/ButtonBase.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "IButton.h"
+#include <chrono>
 
 class ButtonBase : public IButton {
 public:
   explicit ButtonBase(const HardwarePinConfig &config)
-      : IButton(config), _isPressed(false) {}
+      : IButton(config), _isPressed(false), _lastPressTime(),
+        _autoRepeatSettings() {}
 
   virtual bool isPressed() const override { return _isPressed; }
 
@@ -15,7 +17,17 @@ public:
 
   virtual void update() = 0; // Must be implemented in derived classes
 
+  // Implement setAutoRepeatSettings
+  void setAutoRepeatSettings(
+      const InteractionSettings::AutoRepeat &settings) override {
+    _autoRepeatSettings = settings;
+  }
+
 protected:
   bool _isPressed; // Current pressed state of the Button
   ButtonPressCallback _onPressCallback = nullptr; // Callback for button press
+
+  // Store last press time for auto-repeat logic
+  std::chrono::steady_clock::time_point _lastPressTime;
+  InteractionSettings::AutoRepeat _autoRepeatSettings; // Auto-repeat settings
 };
