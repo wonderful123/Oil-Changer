af646161830dc3ce329bdf72612fb49cd2ee4b26
Josh Jenkins <clearharry@gmail.com>
2023-12-01 10:50:48 +1000

    added interaction settings

--- lib/Config/ConfigManager.cpp
+++ lib/Config/ConfigManager.cpp
@@ -1,7 +1,10 @@
 #include "ConfigManager.h"
 #include "Error.h"
 #include "HardwareConfig.h"
+#include "InteractionSettingsConfig.h"
+#include <map>
 #include <memory>
+#include <string>
 
 // Responsible for loading configuration data from a source (like a JSON file)
 // and passing it to the appropriate managers
@@ -14,23 +17,34 @@ std::shared_ptr<HardwareConfig> ConfigManager::getHardwareConfig() const {
   return nullptr; // Return nullptr to indicate config not found
 }
 
+std::shared_ptr<InteractionSettingsConfig>
+ConfigManager::getInteractionSettingsConfig() const {
+  auto it = configs.find("InteractionSettingsConfig");
+  if (it != configs.end()) {
+    return std::static_pointer_cast<InteractionSettingsConfig>(it->second);
+  }
+  return nullptr; // Return nullptr to indicate config not found
+}
+
 Error ConfigManager::loadConfig(const std::string &configType) {
-  // Check if the config type is recognized
+  std::shared_ptr<BaseConfig> config;
+  std::string filePath;
+
   if (configType == "HardwareConfig") {
-    // Create and load the HardwareConfig object
-    auto hardwareConfig = std::make_shared<HardwareConfig>(_fileHandler);
-    Error error = hardwareConfig->load("/config/hardwareConfig.json");
-
-    // Add the loaded config to the `configs` map if loading was successful
-    if (error == Error::OK) {
-      configs[configType] = hardwareConfig;
-    }
-
-    return error;
+    config = std::make_shared<HardwareConfig>(_fileHandler);
+    filePath = "/config/hardwareConfig.json";
+  } else if (configType == "InteractionSettingsConfig") {
+    config = std::make_shared<InteractionSettingsConfig>(_fileHandler);
+    filePath = "/config/interactionSettingsConfig.json";
   } else {
-    // Handle other config types here
+    return Error(Error::ConfigTypeNotRecognized);
   }
 
-  // Config type not recognized
-  return Error(Error::ConfigTypeNotRecognized);
+  Error error = config->load(filePath);
+  if (error == Error::OK) {
+    configs[configType] = config;
+    notifyObservers(); // Notify observers after updating config
+  }
+
+  return error;
 }
--- lib/Config/ConfigManager.h
+++ lib/Config/ConfigManager.h
@@ -1,13 +1,16 @@
 #pragma once
 
+#include "Core/ISubject.h"
 #include "Error.h"          // For error handling
 #include "HardwareConfig.h" // Specific hardware configuration class
 #include "IConfig.h"        // Interface for configuration classes
 #include "IFileHandler.h"   // Interface for file handling
+#include "InteractionSettingsConfig.h"
+#include <algorithm>
 #include <memory>
 #include <unordered_map>
 
-class ConfigManager {
+class ConfigManager : public ISubject {
 public:
   explicit ConfigManager(IFileHandler *fileHandler)
       : _fileHandler(fileHandler) {}
@@ -15,10 +18,33 @@ public:
   // Method to get hardware configuration object
   virtual std::shared_ptr<HardwareConfig> getHardwareConfig() const;
 
+  std::shared_ptr<InteractionSettingsConfig>
+  getInteractionSettingsConfig() const;
+
   // Method to load a configuration
   Error loadConfig(const std::string &configType);
+
+  void notifyObservers() {
+    for (auto &observer : observers) {
+      observer->update();
+    }
+  }
+
+  void attach(IObserver *observer) override { observers.push_back(observer); }
+
+  void detach(IObserver *observer) {
+    observers.erase(std::remove(observers.begin(), observers.end(), observer),
+                    observers.end());
+  }
+
+  void notify() {
+    for (auto &observer : observers) {
+      observer->update();
+    }
+  }
 
 private:
   IFileHandler *_fileHandler;
   std::unordered_map<std::string, std::shared_ptr<IConfig>> configs;
+  std::vector<IObserver *> observers;
 };
