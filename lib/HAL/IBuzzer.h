09e2973e41ee250f8191e41b7c1d78ca7d7529b5
Josh Jenkins <clearharry@gmail.com>
2023-12-01 10:53:02 +1000

    added interaction settings

--- lib/HAL/IBuzzer.h
+++ lib/HAL/IBuzzer.h
@@ -2,14 +2,20 @@
 
 #include "HardwareComponent.h"
 #include "HardwarePinConfig.h"
+#include "InteractionSettings.h"
 
 class IBuzzer : public HardwareComponent {
 public:
-  using HardwareComponent::HardwareComponent; // Inherit constructor
-  
+  using HardwareComponent::HardwareComponent;
+
   virtual ~IBuzzer() = default;
+
+  virtual void updateSettings(const InteractionSettings &settings) = 0;
   virtual void beep(int frequency, int duration) = 0;
   virtual void stop() = 0;
   virtual bool isBeeping() const = 0;
   virtual void setVolume(float volume) = 0;
+
+protected:
+  InteractionSettings currentSettings;
 };
