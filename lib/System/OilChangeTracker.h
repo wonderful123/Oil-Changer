688c58e98196dd8468c59d4564b4b145fdc082d5
Josh Jenkins <clearharry@gmail.com>
2023-12-03 16:20:57 +1000

    change to singleton

--- lib/System/OilChangeTracker.h
+++ lib/System/OilChangeTracker.h
@@ -1,12 +1,24 @@
+#pragma once
+
 #include <functional>
 #include <string>
+#include <vector>
+#include "ISubject.h"
 
 class OilChangeTracker : public ISubject {
 public:
+  static OilChangeTracker &getInstance() {
+    static OilChangeTracker instance;
+    return instance;
+  }
+
+  OilChangeTracker(OilChangeTracker const &) = delete;
+  void operator=(OilChangeTracker const &) = delete;
+
   // Constructor
   OilChangeTracker(double fillCapacity)
-      : fillCapacity(fillCapacity), amountFilled(0), amountExtracted(0),
-        flowRateFill(0), flowRateExtract(0), voltage(0) {}
+      : _fillCapacity(fillCapacity), _amountFilled(0), _amountExtracted(0),
+        _flowRateFill(0), _flowRateExtract(0), _voltage(0) {}
 
   // Setters
   void setAmountFilled(double amount);
@@ -14,6 +26,8 @@ public:
   void setFlowRateFill(double rate);
   void setFlowRateExtract(double rate);
   void setVoltage(double volt);
+  void incrementFillCapacity(double amount);
+  void decrementFillCapacity(double amount);
 
   // Getters
   double fillCapacity() const;
@@ -34,12 +48,14 @@ public:
   void notify(EventType eventType);
 
 private:
-  double _fillCapacity;
-  double _amountFilled;
-  double _amountExtracted;
-  double _flowRateFill;    // Current flow rate for filling
-  double _flowRateExtract; // Current flow rate for extraction
-  double _voltage;         // Voltage sense
+  OilChangeTracker() = default;
+
+  double _fillCapacity = 0;
+  double _amountFilled = 0;
+  double _amountExtracted = 0;
+  double _flowRateFill = 0; // Current flow rate for filling
+  double _flowRateExtract = 0; // Current flow rate for extraction
+  double _voltage = 0;         // Voltage sense
 
   std::vector<IObserver *> _observers; // List of observer callbacks
 
