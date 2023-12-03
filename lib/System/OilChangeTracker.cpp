ab0b004370ec66314d2bc3dab7aa5ffb40a50bb2
Josh Jenkins <clearharry@gmail.com>
2023-12-03 16:20:44 +1000

    add increment methods

--- lib/System/OilChangeTracker.cpp
+++ lib/System/OilChangeTracker.cpp
@@ -1,6 +1,7 @@
 // OilChangeTracker.cpp
 
 #include "OilChangeTracker.h" // Include the header file for the class
+#include <algorithm>
 
 // Define the setter methods
 void OilChangeTracker::setAmountFilled(double amount) { _amountFilled = amount; }
@@ -16,6 +17,14 @@ void OilChangeTracker::setFlowRateExtract(double rate) {
 }
 
 void OilChangeTracker::setVoltage(double volt) { _voltage = volt; }
+
+void OilChangeTracker::incrementFillCapacity(double amount) {
+  _fillCapacity += amount;
+}
+
+void OilChangeTracker::decrementFillCapacity(double amount) {
+  _fillCapacity -= amount;
+}
 
 // Define the getter methods
 double OilChangeTracker::fillCapacity() const { return _fillCapacity; }
@@ -40,13 +49,13 @@ bool OilChangeTracker::isFull() const { return _amountFilled >= _fillCapacity; }
 bool OilChangeTracker::isEmpty() const { return _amountFilled == 0; }
 
 // Define the observer pattern integration
-void OilChangeTracker::attach(IObserver *observer) override {
+void OilChangeTracker::attach(IObserver *observer) {
   _observers.push_back(observer);
 }
 
-void OilChangeTracker::detach(IObserver *observer) override {
+void OilChangeTracker::detach(IObserver *observer) {
   _observers.erase(std::remove(_observers.begin(), _observers.end(), observer),
-                  _observers.end());
+                   _observers.end());
 }
 
 void OilChangeTracker::notify(EventType eventType) {
