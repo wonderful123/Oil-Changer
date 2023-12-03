f4c0f3f71391f7d492855b7ead305ee701cae5a2
Josh Jenkins <clearharry@gmail.com>
2023-12-03 11:36:35 +1000

    mock improvement

--- lib/Hardware/Mocks/MockButton.h
+++ lib/Hardware/Mocks/MockButton.h
@@ -8,11 +8,12 @@
 class MockButton : public ButtonBase {
 public:
   explicit MockButton(const HardwarePinConfig &config) : ButtonBase(config) {
-    setInitialized(true); // Assuming ButtonBase has setInitialized
+    setInitialized(true); // Assuming ButtonBase has a setInitialized method
   }
-  
+
   MOCK_METHOD(bool, isPressed, (), (const, override));
-  MOCK_METHOD(void, setOnPressCallback, (ButtonPressCallback callback), (override));
+  MOCK_METHOD(void, setOnPressCallback, (ButtonPressCallback callback),
+              (override));
   MOCK_METHOD(void, update, (), (override));
 };
 
