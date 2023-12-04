7c43b921326c5e70f2bfaf679d45611b7d08a2c7
Josh Jenkins <clearharry@gmail.com>
2023-12-05 06:39:20 +1000

    improved initialization

--- lib/Hardware/ESP32/ESP32Serial.cpp
+++ lib/Hardware/ESP32/ESP32Serial.cpp
@@ -1,18 +1,50 @@
 #ifdef PLATFORM_ESP32
 
 #include "ESP32Serial.h"
+#include "Error.h"
 
 ESP32Serial::ESP32Serial(const HardwarePinConfig &config)
     : ISerial(config), _serial(0), // Default to UART0. Adjust as needed.
-      _rxPin(config.pinNumber),    // Assuming pinNumber is RX
-      _txPin(-1) {                 // TX pin not specified in this example
-  setInitialized(true);            // Set initialized flag to true
+      _rxPin(-1),               // Default values in case they are not provided
+      _txPin(-1) { // Default baud rate
+
+  // Checking if pins are provided in the configuration
+  auto rxIt = config.pins.find("RXD");
+  if (rxIt != config.pins.end()) {
+    _rxPin = rxIt->second;
+  }
+
+  auto txIt = config.pins.find("TXD");
+  if (txIt != config.pins.end()) {
+    _txPin = txIt->second;
+  }
+
+  // Extracting the baud rate if provided
+  auto baudIt = config.options.find("baud");
+  if (baudIt != config.options.end()) {
+    std::istringstream ss(baudIt->second);
+    if (!(ss >> _baud)) { // If conversion fails, default to 9600
+      _baud = 9600;
+    }
+  }
+
+  // Initialize the Serial with extracted configurations
+  if (_rxPin != -1 && _txPin != -1) {
+    begin(_baud);
+    setInitialized(true);
+  } else {
+    Error(Error::SerialHardwareConfigError);
+  }
 }
 
 void ESP32Serial::begin(unsigned long baudrate) {
   if (_txPin > -1) {
     _serial.begin(baudrate, SERIAL_8N1, _rxPin, _txPin);
   } else {
+    if (!Serial) {
+      Logger::error("Serial port not ready");
+      return;
+    }
     _serial.begin(baudrate, SERIAL_8N1, _rxPin);
   }
 }
