761f6a80817f4d89f84ce2e723eff049c3d7a507
Josh Jenkins <clearharry@gmail.com>
2023-12-05 13:52:24 +1000

    handle display config

--- lib/Config/HardwareConfig.h
+++ lib/Config/HardwareConfig.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "BaseConfig.h"
+#include "DisplayConfig.h"
 #include "Error.h"
 #include "HardwarePinConfig.h"
 #include "IFileHandler.h"
@@ -17,8 +18,12 @@ public:
   // Method to get the hardware configurations
   const std::vector<HardwarePinConfig> &getHardwarePinConfigs() const;
 
+  // Add a method to get display configurations
+  const std::vector<DisplayConfig> &getDisplayConfigs() const;
+
 private:
   std::vector<HardwarePinConfig> _hardwarePinConfigs;
+  std::vector<DisplayConfig> _displayConfigs; // Store display configurations
 
   Error parseJson(const DynamicJsonDocument &doc) override;
 
@@ -28,6 +33,8 @@ private:
                                    const std::string &id,
                                    const std::string &type);
   HardwarePinConfig parseMultiPin(const JsonObjectConst &obj,
-                const std::string &id, const std::string &type);
+                                  const std::string &id,
+                                  const std::string &type);
   void parseOptions(const JsonObjectConst &obj, HardwarePinConfig &config);
+  Error parseDisplays(const JsonArrayConst &displayArray); // Added method
 };
