7e8afd481f0e037e0f38bacb5168cd07b7bd1f66
Josh Jenkins <clearharry@gmail.com>
2023-12-05 13:51:55 +1000

    initial commit

---
+++ lib/Config/DisplayConfig.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class DisplayConfig {
+public:
+  std::string id;
+  std::string interfaceId;
+
+  DisplayConfig(const std::string &id, const std::string &interfaceId)
+      : id(id), interfaceId(interfaceId) {}
+};
