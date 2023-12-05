5f951712ac600230c886877c17fcee1c636854e5
Josh Jenkins <clearharry@gmail.com>
2023-12-05 13:54:42 +1000

    add interface display

--- lib/Hardware/HardwareFactory.h
+++ lib/Hardware/HardwareFactory.h
@@ -7,6 +7,8 @@
 #include "IBuzzer.h"
 #include "IDAC.h"
 #include "IDigitalIO.h"
+#include "IDisplay.h"
+#include "ICommunicationInterface.h"
 #include "IFileHandler.h"
 #include "IFlowMeter.h"
 #include "II2C.h"
@@ -15,38 +17,31 @@
 #include "ISerial.h"
 #include <memory>
 
-// Responsible for the actual creation of hardware objects. It receives
-// specifications or configurations from HardwareManager and creates hardware
-// objects based on those specifications.
-
 class HardwareFactory {
 public:
-  virtual ~HardwareFactory() = default;
+    virtual ~HardwareFactory() = default;
 
-  // Singleton getter function
-  static std::shared_ptr<HardwareFactory> &getHardwareFactory();
+    // Singleton getter function
+    static std::shared_ptr<HardwareFactory> &getHardwareFactory();
 
-  virtual std::shared_ptr<HardwareComponent>
-  createComponent(const HardwarePinConfig &config);
+    virtual std::shared_ptr<HardwareComponent>
+    createComponent(const HardwarePinConfig &config);
 
-  // Factory methods for creating hardware components
-  virtual std::unique_ptr<IADC> createADC(const HardwarePinConfig &config) = 0;
-  virtual std::unique_ptr<IButton>
-  createButton(const HardwarePinConfig &config) = 0;
-  virtual std::unique_ptr<IDAC> createDAC(const HardwarePinConfig &config) = 0;
-  virtual std::unique_ptr<IDigitalIO>
-  createDigitalIO(const HardwarePinConfig &config) = 0;
-  virtual std::unique_ptr<IFlowMeter>
-  createFlowMeter(const HardwarePinConfig &config) = 0;
-  virtual std::unique_ptr<IPWM> createPWM(const HardwarePinConfig &config) = 0;
-  virtual std::unique_ptr<IBuzzer>
-  createBuzzer(const HardwarePinConfig &config) = 0;
-  virtual std::unique_ptr<IFileHandler> createFileHandler() = 0;
-  virtual std::unique_ptr<ISPI> createSPI(const HardwarePinConfig &config) = 0;
-  virtual std::unique_ptr<II2C> createI2C(const HardwarePinConfig &config) = 0;
-  virtual std::unique_ptr<ISerial>
-  createSerial(const HardwarePinConfig &config) = 0;
+    // Factory methods for creating hardware components
+    virtual std::unique_ptr<IADC> createADC(const HardwarePinConfig &config) = 0;
+    virtual std::unique_ptr<IButton> createButton(const HardwarePinConfig &config) = 0;
+    virtual std::unique_ptr<IDAC> createDAC(const HardwarePinConfig &config) = 0;
+    virtual std::unique_ptr<IDigitalIO> createDigitalIO(const HardwarePinConfig &config) = 0;
+    virtual std::unique_ptr<IFlowMeter> createFlowMeter(const HardwarePinConfig &config) = 0;
+    virtual std::unique_ptr<IPWM> createPWM(const HardwarePinConfig &config) = 0;
+    virtual std::unique_ptr<IBuzzer> createBuzzer(const HardwarePinConfig &config) = 0;
+    virtual std::unique_ptr<IFileHandler> createFileHandler() = 0;
+    virtual std::unique_ptr<ISPI> createSPI(const HardwarePinConfig &config) = 0;
+    virtual std::unique_ptr<II2C> createI2C(const HardwarePinConfig &config) = 0;
+    virtual std::unique_ptr<ISerial> createSerial(const HardwarePinConfig &config) = 0;
+    virtual std::unique_ptr<IDisplay> createDisplay(const std::string &displayId, ICommunicationInterface &commInterface) = 0;
+    virtual std::unique_ptr<ICommunicationInterface> createCommunicationInterface() = 0;
 
 private:
-  std::shared_ptr<HardwareFactory> _hardwareFactory;
+    std::shared_ptr<HardwareFactory> _hardwareFactory;
 };
