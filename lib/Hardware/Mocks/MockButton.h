77d27063a77f8cdcbe0aff4a3da929e749bd605d
Josh Jenkins <clearharry@gmail.com>
2023-12-21 12:03:22 +1000

    fixed mock for testing

--- lib/Hardware/Mocks/MockButton.h
+++ lib/Hardware/Mocks/MockButton.h
@@ -2,20 +2,40 @@
 
 #ifdef PLATFORM_NATIVE
 
-#include "ButtonBase.h"
 #include <gmock/gmock.h>
 
+#include "ButtonBase.h"
+
 class MockButton : public ButtonBase {
-public:
-  explicit MockButton(const HardwarePinConfig &config) : ButtonBase(config) {
-    setInitialized(true); // Assuming ButtonBase has a setInitialized method
+ public:
+  explicit MockButton(const HardwarePinConfig& config) : ButtonBase(config) {
+    setInitialized(true);  // Assuming ButtonBase has a setInitialized method
   }
 
-  MOCK_METHOD(bool, isPressed, (), (const, override));
-  MOCK_METHOD(void, setOnPressCallback, (ButtonPressCallback callback),
-              (override));
-  MOCK_METHOD(void, update, (), (override));
-  MOCK_METHOD(ButtonBase::ButtonState, getCurrentState, (), (const));
+  // Method to simulate the button being pressed or not
+  void simulatePress(bool pressed) { _isPressed = pressed; }
+
+  MOCK_METHOD(void, updateButtonState, (), (override));
+
+  // This override allows the mock to capture settings applied to it
+  void setAutoRepeatSettings(
+      const InteractionSettings::AutoRepeat& settings) override {
+    currentSettings = settings;
+    ButtonBase::setAutoRepeatSettings(
+        settings);  // Call base class implementation
+  }
+
+  // For testing: Get the applied auto-repeat settings
+  const InteractionSettings::AutoRepeat& getAppliedSettings() const {
+    return currentSettings;
+  }
+
+  void mockAdjustLastPressTime(std::chrono::milliseconds duration) {
+    adjustLastPressTimeForTesting(duration);
+  }
+
+ private:
+  InteractionSettings::AutoRepeat currentSettings;
 };
 
-#endif // PLATFORM_NATIVE
+#endif  // PLATFORM_NATIVE
