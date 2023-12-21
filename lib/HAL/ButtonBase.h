8431c5eb5a1d1477b3bbc08cc3cc9b5078da8e6d
Josh Jenkins <clearharry@gmail.com>
2023-12-21 12:03:08 +1000

    refactored logic and improved state management

--- lib/HAL/ButtonBase.h
+++ lib/HAL/ButtonBase.h
@@ -11,42 +11,32 @@ class ButtonBase : public IButton {
         _isPressed(false),
         _lastPressTime(std::chrono::steady_clock::now()),
         _autoRepeatSettings(),
-        _isInAutoRepeatMode(false) {}
+        _isInAutoRepeatMode(false),
+        _wasPressed(false) {}
 
   virtual bool isPressed() const override { return _isPressed; }
 
-  void setOnPressCallback(ButtonPressCallback callback) override {
+  virtual void setOnPressCallback(ButtonPressCallback callback) override {
     _onPressCallback = callback;
   }
 
-  virtual void update() = 0;  // Must be implemented in derived classes
-
-  void setAutoRepeatSettings(
+  virtual void setAutoRepeatSettings(
       const InteractionSettings::AutoRepeat &settings) override {
     _autoRepeatSettings = settings;
   }
 
-  // Utility method to check for auto-repeat
-  bool checkAutoRepeat() {
-    auto now = std::chrono::steady_clock::now();
-    if (_isPressed) {
-      auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
-                         now - _lastPressTime)
-                         .count();
-      if (!_isInAutoRepeatMode &&
-          elapsed >= _autoRepeatSettings.initialDelayMs) {
-        _isInAutoRepeatMode = true;
-        _lastPressTime = now;
-        return true;
-      } else if (_isInAutoRepeatMode &&
-                 elapsed >= _autoRepeatSettings.standardRateMs) {
-        _lastPressTime = now;
-        return true;
-      }
-    } else {
-      _isInAutoRepeatMode = false;
+  virtual void update() override {
+    updateButtonState();  // Reflect hardware state in _isPressed
+
+    // Handle state change: press or release
+    if (_isPressed != _wasPressed) {
+      handleStateChange();
     }
-    return false;
+
+    // Auto-repeat functionality for held-down button
+    if (_isPressed && shouldAutoRepeat()) {
+      invokePressCallback();
+    }
   }
 
   // Method to get the current state of the button
@@ -59,10 +49,74 @@ class ButtonBase : public IButton {
   }
 
  protected:
-  bool _isPressed;  // Current pressed state of the Button
+  // Must be implemented in derived classes to update the _isPressed state
+  virtual void updateButtonState() = 0;
+
+  bool _isPressed;   // Current pressed state of the Button
+  bool _wasPressed;  // Tracks the previous pressed state
   ButtonPressCallback _onPressCallback = nullptr;  // Callback for button press
   std::chrono::steady_clock::time_point _lastPressTime;
   InteractionSettings::AutoRepeat _autoRepeatSettings;  // Auto-repeat settings
   bool _isInAutoRepeatMode;  // Flag to indicate if the button is in auto-repeat
                              // mode
+
+  // Testing utility: Adjusts the last press time for simulating time passage
+  virtual void adjustLastPressTimeForTesting(
+      std::chrono::milliseconds duration) {
+    _lastPressTime -= duration;
+  }
+
+ private:
+  void invokePressCallback() {
+    if (_onPressCallback) {
+      _onPressCallback(id());
+    }
+  }
+
+  void handleStateChange() {
+    resetLastPressTime();         // Resetting time on state change
+    _isInAutoRepeatMode = false;  // Reset auto-repeat mode on any state change
+
+    if (_isPressed) {
+      // Optionally, invoke the callback only on press
+      invokePressCallback();
+    }
+
+    _wasPressed = _isPressed;
+  }
+
+  void resetLastPressTime() {
+    _lastPressTime = std::chrono::steady_clock::now();
+  }
+
+  void enterAutoRepeatMode() {
+    _isInAutoRepeatMode = true;
+    resetLastPressTime();  // Reset time when entering auto-repeat mode
+  }
+
+  bool shouldAutoRepeat() {
+    auto elapsedTime = getElapsedTime();
+
+    if (!_isInAutoRepeatMode &&
+        elapsedTime >= _autoRepeatSettings.initialDelayMs) {
+      enterAutoRepeatMode();
+      return true;
+    }
+
+    if (_isInAutoRepeatMode &&
+        elapsedTime >= _autoRepeatSettings.standardRateMs) {
+      resetLastPressTime();  // Reset for next interval
+      return true;
+    }
+
+    return false;
+  }
+
+  uint32_t getElapsedTime() const {
+    auto now = std::chrono::steady_clock::now();
+    auto elapsedDuration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(now -
+                                                              _lastPressTime);
+    return static_cast<uint32_t>(elapsedDuration.count());
+  }
 };
