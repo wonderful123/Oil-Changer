2ff4b18ea71d74dc03b491701a58cf1142f21f31
Josh Jenkins <clearharry@gmail.com>
2023-12-22 13:44:10 +1000

    remove unneeded events

--- lib/System/FSM/Events.h
+++ lib/System/FSM/Events.h
@@ -32,32 +32,24 @@ struct OilCapacityUpdatedEvent : OilCapacityEvent {};
 struct OilExtractionEvent : tinyfsm::Event {
   virtual ~OilExtractionEvent() = default;
 };  // Base event for oil extraction
-struct OilExtractionStartedEvent : OilExtractionEvent {};
 struct OilCapacityThresholdReachedEvent : OilExtractionEvent {};
 struct ExtractLowPressureSwitchTriggeredEvent : OilExtractionEvent {};
-struct OilExtractionCompletedEvent : OilExtractionEvent {};
 
 struct InterimEvent : tinyfsm::Event {
   virtual ~InterimEvent() = default;
-};  // Base event for filter replacement
-struct InterimPeriodStartedEvent : InterimEvent {};
-struct InterimPeriodFinishedEvent : InterimEvent {};
+};
 
 struct OilFillingEvent : tinyfsm::Event {
   virtual ~OilFillingEvent() = default;
 };  // Base event for oil filling
-struct OilFillingStartedEvent : OilFillingEvent {};
 struct OilCapacityTargetReachedEvent : OilFillingEvent {};
 struct FillLowPressureSwitchTriggeredEvent : OilFillingEvent {};
-struct OilFillingCompletedEvent : OilFillingEvent {};
 
 // Configuration Mode Events
 struct ConfigurationModeEvent : tinyfsm::Event {
   virtual ~ConfigurationModeEvent() = default;
-};  // Base event for configuration mode
-struct ConfigModeEnteredEvent : ConfigurationModeEvent {};
+};
 struct SettingUpdatedEvent : ConfigurationModeEvent {};
-struct ConfigModeExitedEvent : ConfigurationModeEvent {};
 
 // Additional Events
 struct ErrorDetectedEvent : tinyfsm::Event {};
