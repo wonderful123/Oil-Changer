0dcc5aeb7e35aef21c91458821080c8e4c5204f5
Josh Jenkins <clearharry@gmail.com>
2023-12-22 13:48:29 +1000

    remove colleague

--- lib/HAL/IDisplay.h
+++ lib/HAL/IDisplay.h
@@ -2,16 +2,10 @@
 
 #include <string>
 
-#include "IColleague.h"
-
-class IDisplay : public IColleague {
+class IDisplay {
  public:
   virtual ~IDisplay() {}
 
   // Display data on the screen
   virtual void displayData(const std::string &data) = 0;
-
-  // Mediator pattern integration
-  virtual void setMediator(std::shared_ptr<IMediator> mediator) = 0;
-  virtual void receiveEvent(EventType eventType, const std::string &data) = 0;
 };
