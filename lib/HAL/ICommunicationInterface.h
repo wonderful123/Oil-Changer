57f1e7cf53568dcd523a1a555b6b74954da184bc
Josh Jenkins <clearharry@gmail.com>
2023-12-22 13:41:31 +1000

    missing include and constructor update

--- lib/HAL/ICommunicationInterface.h
+++ lib/HAL/ICommunicationInterface.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <string>
+
 class ICommunicationInterface {
-public:
-  virtual ~ICommunicationInterface() {}
+ public:
+  virtual ~ICommunicationInterface() = default;
 
   virtual void send(const std::string &message) = 0;
-  virtual std::string receive() = 0; // Depending on your needs
+  virtual std::string receive() = 0;  // Depending on your needs
 };
