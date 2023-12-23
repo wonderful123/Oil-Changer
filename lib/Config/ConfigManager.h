24b542424f1a6139422164dd122c8a652342896c
Josh Jenkins <clearharry@gmail.com>
2023-12-23 12:19:00 +1000

    remove forward declaration

--- lib/Config/ConfigManager.h
+++ lib/Config/ConfigManager.h
@@ -1,34 +1,35 @@
 #pragma once
 
-#include "../Mediator/IColleague.h"
 #include <memory>
 #include <string>
 #include <unordered_map>
 
+#include "../Mediator/IColleague.h"
+#include "HardwareConfig.h"
+
 // Forward declarations
 class IConfig;
 class Error;
-class HardwareConfig;
 class InteractionSettingsConfig;
 class IFileHandler;
 
 class ConfigManager : public IColleague {
-public:
+ public:
   ConfigManager(std::shared_ptr<IMediator> mediator,
                 std::shared_ptr<IFileHandler> fileHandler);
 
   // Method to get hardware configuration object
   virtual std::shared_ptr<HardwareConfig> getHardwareConfig() const;
 
-  std::shared_ptr<InteractionSettingsConfig>
-  getInteractionSettingsConfig() const;
+  virtual std::shared_ptr<InteractionSettingsConfig> getInteractionSettingsConfig()
+      const;
 
   // Method to load a configuration
   Error loadConfig(const std::string &configType);
 
   void receiveEvent(EventType eventType, const EventData *eventData) override;
 
-private:
+ private:
   std::shared_ptr<IFileHandler> _fileHandler;
   std::unordered_map<std::string, std::shared_ptr<IConfig>> configs;
   std::shared_ptr<IMediator> _mediator;
