2748b559d945930d87fb002bc1f2e312f6b029be
Josh Jenkins <clearharry@gmail.com>
2023-12-23 12:19:55 +1000

    adjust write interface

--- lib/HAL/ISerial.h
+++ lib/HAL/ISerial.h
@@ -15,7 +15,7 @@ class ISerial : public HardwareComponent {
 
   virtual void begin(unsigned long baudrate) = 0;
   virtual void end() = 0;
-  virtual size_t write(uint8_t byte) = 0;
+  virtual void write(const std::string &message) = 0;
   virtual int read() = 0;
   virtual int available() = 0;
   virtual void flush() = 0;
