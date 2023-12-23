00ec108564ee68804b72f36a670068ea71c1a449
Josh Jenkins <clearharry@gmail.com>
2023-12-23 12:22:39 +1000

    new errors for hardware initialization

--- lib/Utils/Error.h
+++ lib/Utils/Error.h
@@ -5,50 +5,60 @@
 #include <string>
 
 // Macro list of error codes and their messages
-#define ERROR_CODE_DEFINITIONS                                                 \
-  ERROR_CODE(OK, "No Error.")                                                  \
-  ERROR_CODE(FileSystemMountFailure, "Failed to mount the file system.")       \
-  ERROR_CODE(FileOpenFailure, "Unable to open the specified file.")            \
-  ERROR_CODE(FileReadFailure, "Error encountered while reading the file.")     \
-  ERROR_CODE(FileSystemMountError, "Failed to mount the file system.")         \
-  ERROR_CODE(FileWriteFailure, "Error encountered while writing the file.")    \
-  ERROR_CODE(HardwareConfigComponentsKeyMissing,                               \
-             "Missing 'components' key in hardware configuration.")            \
-  ERROR_CODE(HardwareConfigInitError,                                          \
-             "Failed to initialize the hardware configuration.")               \
-  ERROR_CODE(HardwareConfigBuzzerInitError,                                    \
-             "Failed to initialize the buzzer.")                               \
-  ERROR_CODE(JsonInputInvalid,                                                 \
-             "The provided JSON input is invalid or malformed.")               \
-  ERROR_CODE(JsonMemoryError,                                                  \
-             "Insufficient memory to process the JSON document.")              \
-  ERROR_CODE(JsonInputEmpty, "The JSON input provided is empty.")              \
-  ERROR_CODE(JsonInputIncomplete,                                              \
-             "The JSON input is incomplete or ends abruptly.")                 \
-  ERROR_CODE(JsonDeserializationFailure,                                       \
-             "Failed to deserialize the JSON document due to an error.")       \
-  ERROR_CODE(                                                                  \
-      ConfigTypeNotRecognized,                                                 \
-      "The specified configuration type is not recognized or supported.")      \
-  ERROR_CODE(DigitalIOModeOptionMissingOrInvalid,                              \
-             "Missing or invalid mode in DigitalIO options")                   \
-  ERROR_CODE(ADCInvalidAttenuationValue, "Invalid ADC attenuation value.")     \
-  ERROR_CODE(FlowMeterInitErrorNoPCNTUnitSpecified,                            \
-             "FlowMeter: No PCNT unit specified for GPIO pin")                 \
-  ERROR_CODE(InteractionSettingsButtonInteractionKeyMissing,                   \
-             "Missing 'buttonInteraction' key in interactionSettings.json")    \
-  ERROR_CODE(InteractionSettingsButtonsSubkeyMissing,                          \
-             "Missing 'buttons' subkey in interactionsSettings.json")          \
-  ERROR_CODE(InteractionSettingsBeepSettingsSubkeyMissing,                     \
-             "Missing 'beep' subkey in interactionSettings.json")              \
-  ERROR_CODE(InteractionSettingsFeedbackSubkeyMissing,                         \
-             "Missing 'feedback' subkey in interactionSettings.json")          \
-  ERROR_CODE(SerialHardwareConfigError,                                        \
-             "Failed to initialize the serial hardware configuration.")        \
-  ERROR_CODE(SerialInvalidUartPortNumber,                                      \
-             "ESP32 Serial: Invalid UART port number for ESP32.")              \
-  ERROR_CODE(InvalidMediatorPassedToIColleague,                                \
-             "Invalid mediator passed to IColleague.")                         \
+#define ERROR_CODE_DEFINITIONS                                              \
+  ERROR_CODE(OK, "No Error.")                                               \
+  ERROR_CODE(FileSystemMountFailure, "Failed to mount the file system.")    \
+  ERROR_CODE(FileOpenFailure, "Unable to open the specified file.")         \
+  ERROR_CODE(FileReadFailure, "Error encountered while reading the file.")  \
+  ERROR_CODE(FileSystemMountError, "Failed to mount the file system.")      \
+  ERROR_CODE(FileWriteFailure, "Error encountered while writing the file.") \
+  ERROR_CODE(HardwareConfigComponentsKeyMissing,                            \
+             "Missing 'components' key in hardware configuration.")         \
+  ERROR_CODE(HardwareConfigInitError,                                       \
+             "Failed to initialize the hardware configuration.")            \
+  ERROR_CODE(HardwareConfigBuzzerInitError,                                 \
+             "Failed to initialize the buzzer.")                            \
+  ERROR_CODE(JsonInputInvalid,                                              \
+             "The provided JSON input is invalid or malformed.")            \
+  ERROR_CODE(JsonMemoryError,                                               \
+             "Insufficient memory to process the JSON document.")           \
+  ERROR_CODE(JsonInputEmpty, "The JSON input provided is empty.")           \
+  ERROR_CODE(JsonInputIncomplete,                                           \
+             "The JSON input is incomplete or ends abruptly.")              \
+  ERROR_CODE(JsonDeserializationFailure,                                    \
+             "Failed to deserialize the JSON document due to an error.")    \
+  ERROR_CODE(                                                               \
+      ConfigTypeNotRecognized,                                              \
+      "The specified configuration type is not recognized or supported.")   \
+  ERROR_CODE(DigitalIOModeOptionMissingOrInvalid,                           \
+             "Missing or invalid mode in DigitalIO options")                \
+  ERROR_CODE(ADCInvalidAttenuationValue, "Invalid ADC attenuation value.")  \
+  ERROR_CODE(FlowMeterInitErrorNoPCNTUnitSpecified,                         \
+             "FlowMeter: No PCNT unit specified for GPIO pin")              \
+  ERROR_CODE(InteractionSettingsButtonInteractionKeyMissing,                \
+             "Missing 'buttonInteraction' key in interactionSettings.json") \
+  ERROR_CODE(InteractionSettingsButtonsSubkeyMissing,                       \
+             "Missing 'buttons' subkey in interactionsSettings.json")       \
+  ERROR_CODE(InteractionSettingsBeepSettingsSubkeyMissing,                  \
+             "Missing 'beep' subkey in interactionSettings.json")           \
+  ERROR_CODE(InteractionSettingsFeedbackSubkeyMissing,                      \
+             "Missing 'feedback' subkey in interactionSettings.json")       \
+  ERROR_CODE(SerialHardwareConfigError,                                     \
+             "Failed to initialize the serial hardware configuration.")     \
+  ERROR_CODE(SerialInvalidUartPortNumber,                                   \
+             "ESP32 Serial: Invalid UART port number for ESP32.")           \
+  ERROR_CODE(InvalidMediatorPassedToIColleague,                             \
+             "Invalid mediator passed to IColleague.")                      \
+  ERROR_CODE(HardwareComponentCreationFailure,                              \
+             "Failed to create a hardware component.")                      \
+  ERROR_CODE(ButtonRegistrationFailure,                                     \
+             "Failed to register a button component.")                      \
+  ERROR_CODE(BuzzerManagerInitializationFailure,                            \
+             "Failed to initialize BuzzerManager.")                         \
+  ERROR_CODE(ConfigManagerInteractionSettingsError,                         \
+             "Failed to return interaction settings from config manager.")  \
+  ERROR_CODE(ConfigManagerHardwareConfigNotFound,                           \
+             "Failed to return hardware configuration from config manager.")\
 
 #undef ERROR_CODE
 
