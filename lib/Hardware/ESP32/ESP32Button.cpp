8175fea2d44d704eb8e1965fc385e162c4335463
Josh Jenkins <clearharry@gmail.com>
2023-12-24 20:25:52 +1000

    fix debounce code

--- lib/Hardware/ESP32/ESP32Button.cpp
+++ lib/Hardware/ESP32/ESP32Button.cpp
@@ -1,18 +1,23 @@
 #ifdef PLATFORM_ESP32
 
 #include "ESP32Button.h"
-#include "Logger.h"
+
 #include <Arduino.h>
+
+#include "Logger.h"
 
 ESP32Button::ESP32Button(const HardwarePinConfig &config) : ButtonBase(config) {
   pinMode(config.pinNumber, INPUT_PULLUP);
+  _debouncer = Bounce();
+  _debouncer.interval(50);  // Debounce interval
   _debouncer.attach(config.pinNumber);
-  _debouncer.interval(50); // Debounce interval
 }
 
 void ESP32Button::updateButtonState() {
   _debouncer.update();
-  _isPressed = _debouncer.read() == LOW;  // Assuming active LOW
+  if (_debouncer.changed()) {
+    _isPressed = !_debouncer.read();
+  }
 }
 
-#endif // PLATFORM_ESP32
+#endif  // PLATFORM_ESP32
