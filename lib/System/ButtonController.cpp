f5cef0f1a03ac3277365ac25fceeec99b593cce1
Josh Jenkins <clearharry@gmail.com>
2023-12-28 14:28:23 +1000

    refactor observer and autorepeat

--- lib/System/ButtonController.cpp
+++ lib/System/ButtonController.cpp
@@ -1,48 +1,24 @@
 #include "ButtonController.h"
+#include "Logger.h"
 
-#include <algorithm>
-
-#include "IButton.h"
-#include "InteractionSettings.h"
-#include "Mediator/ConcreteMediator.h"
-
-ButtonController::ButtonController(std::shared_ptr<IMediator> mediator)
-    : IColleague(mediator), _mediator(mediator) {
-  _mediator->registerColleague(this);
-}
+ButtonController::ButtonController(const InteractionSettings &settings)
+    : _settings(settings) {}
 
 void ButtonController::registerButton(const std::string &id,
                                       std::shared_ptr<IButton> button) {
-  // Check if the button is already registered
   if (_buttons.find(id) == _buttons.end()) {
-    // Add the button
     _buttons[id] = button;
-
-    // Set a callback for the button
-    button->setOnPressCallback([this](const std::string &buttonId) {
-      this->handleButtonPress(buttonId);
-    });
-
-    // Apply settings to the button if available
-    if (_settings.buttons.find(id) != _settings.buttons.end()) {
-      button->setAutoRepeatSettings(_settings.buttons.at(id).autoRepeat);
-    }
-
+    applySettingsToButton(button, id);
     Logger::info("[ButtonController] Registered: " + id);
   }
 }
 
-void ButtonController::handleButtonPress(const std::string &id) {
-  notifyMediator(id, EventType::BUTTON_PRESSED);
-  Logger::info("ButtonController::handleButtonPress: Button pressed: " + id);
-}
-
-void ButtonController::notifyMediator(const std::string &id,
-                                      EventType eventType) {
-  if (_mediator) {
-    EventData eventData;
-    eventData.id = id;
-    _mediator->notify(this, eventType, &eventData);
+void ButtonController::applySettingsToButton(std::shared_ptr<IButton> button,
+                                             const std::string &id) {
+  auto settingsIter = _settings.buttons.find(id);
+  if (settingsIter != _settings.buttons.end()) {
+    // If your IButton class supports setting auto-repeat settings, apply them
+    // here. button->setAutoRepeatSettings(settingsIter->second.autoRepeat);
   }
 }
 
@@ -51,45 +27,36 @@ void ButtonController::processButtonStates() {
     const auto &id = pair.first;
     auto &button = pair.second;
 
-    button->update(); // Update the state of the button
-    auto state = button->getCurrentState(); // Get the current state
-    handleAutoRepeat(id, state); // Handle auto-repeat logic if necessary
+    auto prevState = button->getPreviousState(); // Ensure this method is
+                                                 // implemented in IButton
+    auto currentState = button->getCurrentState();
+
+    if (!prevState.isPressed && currentState.isPressed) {
+      notify("button_pressed", id);
+    } else if (prevState.isPressed && !currentState.isPressed) {
+      notify("button_released", id);
+    }
+    // Handle other states and transitions as needed
   }
+}
+
+void ButtonController::notify(const std::string &event, const std::string &id) {
+  for (const auto &observer : observers) {
+    observer->onNotify(event, id);
+  }
+}
+
+void ButtonController::attach(std::shared_ptr<IObserver> observer) {
+  observers.push_back(observer);
+}
+
+void ButtonController::detach(std::shared_ptr<IObserver> observer) {
+  observers.erase(std::remove(observers.begin(), observers.end(), observer),
+                  observers.end());
 }
 
 std::shared_ptr<IButton>
 ButtonController::getButtonById(const std::string &id) const {
   auto it = _buttons.find(id);
   return it != _buttons.end() ? it->second : nullptr;
-}
-
-void ButtonController::setInteractionSettings(
-    const InteractionSettings &settings) {
-  _settings = settings;
-  // Update settings for each registered button
-  for (auto it = _buttons.begin(); it != _buttons.end(); ++it) {
-    auto id = it->first;
-    auto &button = it->second;
-    auto buttonSettingsIter = _settings.buttons.find(id);
-    if (buttonSettingsIter != _settings.buttons.end()) {
-      button->setAutoRepeatSettings(buttonSettingsIter->second.autoRepeat);
-    }
-  }
-}
-
-void ButtonController::handleAutoRepeat(const std::string &id,
-                                        const IButton::ButtonState &state) {
-  if (state.isPressed && state.isInAutoRepeatMode) {
-    // If the button is pressed and in auto-repeat mode, notify the mediator
-    //    notifyMediator(id, EventType::BUTTON_AUTO_REPEAT);
-    Logger::info("[ButtonController] handleAutoRepeat: " + id + " " +
-                 std::to_string(state.isPressed) + " " +
-                 std::to_string(state.isInAutoRepeatMode));
-  }
-}
-
-void ButtonController::receiveEvent(EventType eventType,
-                                    const EventData *eventData) {
-  // Handle the event
-  // Implementation depends on how ButtonController should react to events
 }
--- lib/System/ButtonController.h
+++ lib/System/ButtonController.h
@@ -7,35 +7,26 @@
 
 #include "IButton.h"
 #include "InteractionSettings.h"
-#include "Mediator/IColleague.h"
+#include "Observer/IObserver.h"
+#include "Observer/ISubject.h"
 
-class ButtonController : public IColleague {
- public:
-  struct ButtonState {
-    std::shared_ptr<IButton> button;
-    bool isPressed;
-    std::chrono::steady_clock::time_point lastPressTime;
-    std::chrono::steady_clock::time_point lastRepeatTime;
-    bool isInAutoRepeatMode;
-  };
-
-  ButtonController(std::shared_ptr<IMediator> mediator);
-
+class ButtonController : public ISubject {
+public:
+  ButtonController(const InteractionSettings &settings);
   virtual void registerButton(const std::string &id,
                               std::shared_ptr<IButton> button);
   virtual void processButtonStates();
   virtual std::shared_ptr<IButton> getButtonById(const std::string &id) const;
-  virtual void setInteractionSettings(const InteractionSettings &settings);
 
-  virtual void receiveEvent(EventType eventType,
-                            const EventData *eventData) override;
+  void attach(std::shared_ptr<IObserver> observer) override;
+  void detach(std::shared_ptr<IObserver> observer) override;
+  void notify(const std::string &event, const std::string &id) override;
 
- private:
+private:
   std::unordered_map<std::string, std::shared_ptr<IButton>> _buttons;
-  std::shared_ptr<IMediator> _mediator;
+  std::vector<std::shared_ptr<IObserver>> observers;
   InteractionSettings _settings;
 
-  void handleButtonPress(const std::string &id);
-  void notifyMediator(const std::string &id, EventType eventType);
-  void handleAutoRepeat(const std::string &id, const IButton::ButtonState &state);
+  void applySettingsToButton(std::shared_ptr<IButton> button,
+                             const std::string &id);
 };
