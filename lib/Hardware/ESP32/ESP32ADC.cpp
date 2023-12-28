7d2f065930c389c3fc516226804fae48467cd079
Josh Jenkins <clearharry@gmail.com>
2023-12-28 12:45:07 +1000

    remove unneeded logging

--- lib/Hardware/ESP32/ESP32ADC.cpp
+++ lib/Hardware/ESP32/ESP32ADC.cpp
@@ -3,27 +3,23 @@
 #include "ESP32ADC.h"
 #include "Logger.h"
 
-ESP32ADC::ESP32ADC(const HardwarePinConfig &config)
-    : ADCBase(config) {
-    configureAttenuation(config);
-    setInitialized(true); // Mark ADC as initialized
+ESP32ADC::ESP32ADC(const HardwarePinConfig &config) : ADCBase(config) {
+  configureAttenuation(config);
+  setInitialized(true); // Mark ADC as initialized
 }
 
-int ESP32ADC::read() const {
-    return analogRead(_pinNumber);
-}
+int ESP32ADC::read() const { return analogRead(_pinNumber); }
 
 void ESP32ADC::configureAttenuation(const HardwarePinConfig &config) {
-    int attenuationValue = config.getOptionAs<int>("attenuation");
-    if (attenuationValue >= 0 && attenuationValue <= 3) {
-      _attenuation = static_cast<adc_attenuation_t>(attenuationValue);
-      analogSetAttenuation(_attenuation);
-      Logger::info("[ESP32ADC] ADC attenuation set to " +
-                   std::to_string(attenuationValue));
-    } else {
-        Error(ADCInvalidAttenuationValue);
-        Logger::error("[ESP32ADC] Invalid ADC attenuation value on pin: " + std::to_string(config.pinNumber));
-        setInitialized(false);
-    }
+  int attenuationValue = config.getOptionAs<int>("attenuation");
+  if (attenuationValue >= 0 && attenuationValue <= 3) {
+    _attenuation = static_cast<adc_attenuation_t>(attenuationValue);
+    analogSetAttenuation(_attenuation);
+  } else {
+    Error(ADCInvalidAttenuationValue);
+    Logger::error("[ESP32ADC] Invalid ADC attenuation value on pin: " +
+                  std::to_string(config.pinNumber));
+    setInitialized(false);
+  }
 }
 #endif // PLATFORM_ESP32
