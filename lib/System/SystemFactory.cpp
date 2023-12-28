521b03e86b6a2d0c34490689288beca980fa4fdd
Josh Jenkins <clearharry@gmail.com>
2023-12-28 12:47:29 +1000

    improved logging

--- lib/System/SystemFactory.cpp
+++ lib/System/SystemFactory.cpp
@@ -1,53 +1,52 @@
 // SystemFactory.cpp
 #include "SystemFactory.h"
 
-#include "HardwareFactory.h"
 #include "Logger.h"
 
 SystemFactory::SystemFactory(std::shared_ptr<IFileHandler> fileHandler)
-    : fileHandler(fileHandler) {
-  mediator = std::make_shared<ConcreteMediator>();  // Create a single mediator
-                                                    // instance
-  initializeSystem();
+    : _fileHandler(fileHandler) {
+  _mediator = std::make_shared<ConcreteMediator>();
 }
 
 void SystemFactory::initializeSystem() {
+  Logger::info("[SystemFactory] Creating ConfigManager...");
   createConfigManager();
 
+  Logger::info("[SystemFactory] Creating HardwareManager...");
   createHardwareManager();
-  hardwareManager->initialize();
+  _hardwareManager->initialize();
 
+  Logger::info("[SystemFactory] Creating SystemController...");
   createSystemController();
-  systemController->initializeSystemComponents();
+  _systemController->initializeSystemComponents();
 
-  auto oilChangeTracker =
-      std::make_shared<OilChangeTracker>(mediator, configManager);
-  Logger::info("[SystemFactory] OilChangeTracker initialized.");
-  Logger::info("[SystemFactory] System components initialized.");
+  auto _oilChangeTracker =
+      std::make_shared<OilChangeTracker>(_mediator, _configManager);
+  Logger::info("[SystemFactory] OilChangeTracker initialized");
+  Logger::info("[SystemFactory] All system components initialized");
 }
 
 std::shared_ptr<SystemController> SystemFactory::getSystemController() {
-  return systemController;
+  return _systemController;
 }
 
 std::shared_ptr<HardwareManager> SystemFactory::getHardwareManager() {
-  return hardwareManager;
+  return _hardwareManager;
 }
 
 std::shared_ptr<ConfigManager> SystemFactory::getConfigManager() {
-  return configManager;
+  return _configManager;
 }
 
 void SystemFactory::createConfigManager() {
-  configManager = std::make_shared<ConfigManager>(mediator, fileHandler);
+  _configManager = std::make_shared<ConfigManager>(_mediator, _fileHandler);
 }
 
 void SystemFactory::createHardwareManager() {
-  hardwareManager = std::make_shared<HardwareManager>(configManager);
+  _hardwareManager = std::make_shared<HardwareManager>(_configManager);
 }
 
 void SystemFactory::createSystemController() {
-  auto mediator = std::make_shared<ConcreteMediator>();
-  systemController = std::make_shared<SystemController>(
-      mediator, hardwareManager, configManager);
+  _systemController = std::make_shared<SystemController>(
+      _mediator, _hardwareManager, _configManager);
 }
