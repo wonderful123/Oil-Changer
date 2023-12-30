ee1718b1f907fab876f5ff166e60fc0a62b9bdce
Josh Jenkins <clearharry@gmail.com>
2023-12-30 10:43:56 +1000

    moved code from states.h

--- lib/System/FSM/ReadyState.h
+++ lib/System/FSM/ReadyState.h
@@ -3,40 +3,64 @@
 #include "Events.h"
 #include "IBuzzer.h"
 #include "Observer/IObserver.h"
+#include "Observer/ISubject.h"
 #include "OilChangeTracker.h"
 #include "StateMachine.h"
+#include "SystemFactory.h"
 #include <memory>
 
 class StateMachine;
+class Extracting;
 
-class Ready : public StateMachine, public IObserver {
+class Ready : public StateMachine,
+              public IObserver,
+              public std::enable_shared_from_this<Ready> {
 public:
-  Ready(std::shared_ptr<Buzzer> buzzer,
+  Ready(std::shared_ptr<IBuzzer> buzzer,
         std::shared_ptr<OilChangeTracker> tracker)
-      : _buzzer(buzzer), _tracker(tracker) {}
+      : _buzzer(buzzer), _tracker(tracker) {
+        _buttonController = SystemFactory::getInstance().getButtonController();
+      }
 
   void entry() override {
     StateMachine::entry();
-    setupBuzzer();
+    _buttonController->attach(
+        std::dynamic_pointer_cast<IObserver>(shared_from_this()));
   }
 
   void exit() override {
-    _buzzer->stop(); // Stop the buzzer when exiting the Ready state
+    _buttonController->detach(
+        std::dynamic_pointer_cast<IObserver>(shared_from_this()));
     StateMachine::exit();
   }
 
   void onNotify(const std::string &event, const std::string &id) override {
-    if (event == "buzzer_beep") {
-      _tracker->incrementFillCapacity(0.1);
+    if (event == "button_pressed") {
+      handleButtonPress(id);
+    }
+  }
+
+  void handleButtonPress(const std::string &id) {
+    auto &oilChangeTracker = OilChangeTracker::getInstance();
+    if (id == "ButtonPlus") {
+      oilChangeTracker.incrementFillCapacity(0.1);
+      Logger::info("[StateMachine: Ready] Fill capacity incremented.");
+    } else if (id == "ButtonMinus") {
+      oilChangeTracker.decrementFillCapacity(0.1);
+      Logger::info("[StateMachine: Ready] Fill capacity decremented.");
+    } else if (id == "ButtonStart") {
+      // Start oil change. Transit to extracting state
+      Logger::info("[StateMachine: Ready] Starting oil change.");
+      transit<Extracting>();
     }
   }
 
 private:
   std::shared_ptr<IBuzzer> _buzzer;
   std::shared_ptr<OilChangeTracker> _tracker;
+  std::shared_ptr<ButtonController> _buttonController;
 
   void setupBuzzer() {
     _buzzer->setOnBeepCallback([this]() { notify("buzzer_beep", ""); });
-    _buzzer->startAutoRepeat(); // Start the auto-repeat beeping
   }
 };
