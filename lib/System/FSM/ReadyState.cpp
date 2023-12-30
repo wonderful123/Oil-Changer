14e00c2a47a1a4a5ca6e4128adb380abbd796f91
Josh Jenkins <clearharry@gmail.com>
2023-12-30 13:55:20 +1000

    refactored and removed redundant code

---
+++ lib/System/FSM/ReadyState.cpp
@@ -0,0 +1,52 @@
+#include "ReadyState.h"
+
+#include "Logger.h"
+#include "States.h"
+
+Ready::Ready() {
+  auto &systemFactory = SystemFactory::getInstance();
+  _buzzerManager = systemFactory.getSystemController()->getBuzzerManager();
+  _buttonController = systemFactory.getButtonController();
+}
+
+void Ready::entry() {
+  StateMachine::entry();
+  // Attach to the button controller to listen for button presses
+  _buttonController->attach(
+      std::dynamic_pointer_cast<IObserver>(shared_from_this()));
+
+  setupBuzzerRapidBeepCallback();
+}
+
+void Ready::exit() {
+  _buttonController->detach(
+      std::dynamic_pointer_cast<IObserver>(shared_from_this()));
+
+  // Clear the callback for rapid beep
+  _buzzerManager->setOnRapidBeepCallback(nullptr);
+
+  StateMachine::exit();
+}
+
+void Ready::handleButtonPress(const std::string &id) {
+  auto &oilChangeTracker = OilChangeTracker::getInstance();
+
+  if (id == "ButtonPlus") {
+    // Notify the mediator to increment the fill capacity
+
+    Logger::info("[StateMachine: Ready] Fill capacity incremented.");
+  } else if (id == "ButtonMinus") {
+    // Notify the mediator to decrement the fill capacity
+    Logger::info("[StateMachine: Ready] Fill capacity decremented.");
+  } else if (id == "ButtonStart") {
+    Logger::info("[StateMachine: Ready] Starting oil change.");
+    transit<Extracting>();
+  }
+}
+
+void Ready::setupBuzzerRapidBeepCallback() {
+  _buzzerManager->setOnRapidBeepCallback([this]() {
+    // TODO: Set callback for rapid beep. This should increment or decrement the
+    // fill capacity.
+  });
+}
--- lib/System/FSM/ReadyState.h
+++ lib/System/FSM/ReadyState.h
@@ -1,11 +1,8 @@
 #include <tinyfsm.hpp>
 
-#include "Events.h"
-#include "IBuzzer.h"
 #include "Observer/IObserver.h"
-#include "Observer/ISubject.h"
-#include "OilChangeTracker.h"
-#include "StateMachine.h"
+#include "ButtonController.h"
+#include "BuzzerManager.h"
 #include "SystemFactory.h"
 #include <memory>
 
@@ -16,51 +13,13 @@ class Ready : public StateMachine,
               public IObserver,
               public std::enable_shared_from_this<Ready> {
 public:
-  Ready(std::shared_ptr<IBuzzer> buzzer,
-        std::shared_ptr<OilChangeTracker> tracker)
-      : _buzzer(buzzer), _tracker(tracker) {
-        _buttonController = SystemFactory::getInstance().getButtonController();
-      }
-
-  void entry() override {
-    StateMachine::entry();
-    _buttonController->attach(
-        std::dynamic_pointer_cast<IObserver>(shared_from_this()));
-  }
-
-  void exit() override {
-    _buttonController->detach(
-        std::dynamic_pointer_cast<IObserver>(shared_from_this()));
-    StateMachine::exit();
-  }
-
-  void onNotify(const std::string &event, const std::string &id) override {
-    if (event == "button_pressed") {
-      handleButtonPress(id);
-    }
-  }
-
-  void handleButtonPress(const std::string &id) {
-    auto &oilChangeTracker = OilChangeTracker::getInstance();
-    if (id == "ButtonPlus") {
-      oilChangeTracker.incrementFillCapacity(0.1);
-      Logger::info("[StateMachine: Ready] Fill capacity incremented.");
-    } else if (id == "ButtonMinus") {
-      oilChangeTracker.decrementFillCapacity(0.1);
-      Logger::info("[StateMachine: Ready] Fill capacity decremented.");
-    } else if (id == "ButtonStart") {
-      // Start oil change. Transit to extracting state
-      Logger::info("[StateMachine: Ready] Starting oil change.");
-      transit<Extracting>();
-    }
-  }
+  void entry() override;
+  void exit() override;
 
 private:
-  std::shared_ptr<IBuzzer> _buzzer;
-  std::shared_ptr<OilChangeTracker> _tracker;
   std::shared_ptr<ButtonController> _buttonController;
+  std::shared_ptr<BuzzerManager> _buzzerManager;
 
-  void setupBuzzer() {
-    _buzzer->setOnBeepCallback([this]() { notify("buzzer_beep", ""); });
-  }
+  void handleButtonPress(const std::string &id);
+  void setupBuzzerRapidBeepCallback();
 };
