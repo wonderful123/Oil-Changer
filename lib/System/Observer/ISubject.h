c15c19ae4e00eb3eaaf9b78eefd26fa019b47b86
Josh Jenkins <clearharry@gmail.com>
2024-01-01 11:46:31 +1000

    change to raw pointer

--- lib/System/Observer/ISubject.h
+++ lib/System/Observer/ISubject.h
@@ -1,13 +1,12 @@
 #pragma once
 
 #include "IObserver.h"
-#include <memory>
 #include <vector>
 
 class ISubject {
 public:
   virtual ~ISubject() = default;
-  virtual void attach(std::shared_ptr<IObserver> observer) = 0;
-  virtual void detach(std::shared_ptr<IObserver> observer) = 0;
+  virtual void attach(IObserver *observer) = 0;
+  virtual void detach(IObserver *observer) = 0;
   virtual void notify(const std::string &event, const std::string &id) = 0;
 };
