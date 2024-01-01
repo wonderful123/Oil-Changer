eba25c422b5cbe41826c87922b14292dfdc929d5
Josh Jenkins <clearharry@gmail.com>
2024-01-01 11:44:55 +1000

    add initialization complete event

--- lib/System/FSM/StateMachine.h
+++ lib/System/FSM/StateMachine.h
@@ -5,21 +5,22 @@
 #include "Events.h"
 
 class StateMachine : public tinyfsm::Fsm<StateMachine> {
- public:
+public:
   // Default reaction for unhandled events
-  void react(tinyfsm::Event const&) {}
+  void react(tinyfsm::Event const &) {}
 
   // Reaction functions for base event types
-  virtual void react(ButtonPressEvent const&) {}
-  virtual void react(InitializationEvent const&) {}
-  virtual void react(OilCapacityEvent const&) {}
-  virtual void react(OilExtractionEvent const&) {}
-  virtual void react(OilFillingEvent const&) {}
-  virtual void react(ConfigurationModeEvent const&) {}
-  virtual void react(ErrorDetectedEvent const&) {}
-  virtual void react(UserCancelledEvent const&) {}
-  virtual void react(TimeoutEvent const&) {}
-  virtual void react(MaintenanceNeededEvent const&) {}
+  virtual void react(ButtonPressEvent const &) {}
+  virtual void react(InitializationEvent const &) {}
+  virtual void react(InitializationCompleteEvent const &) {}
+  virtual void react(OilCapacityEvent const &) {}
+  virtual void react(OilExtractionEvent const &) {}
+  virtual void react(OilFillingEvent const &) {}
+  virtual void react(ConfigurationModeEvent const &) {}
+  virtual void react(ErrorDetectedEvent const &) {}
+  virtual void react(UserCancelledEvent const &) {}
+  virtual void react(TimeoutEvent const &) {}
+  virtual void react(MaintenanceNeededEvent const &) {}
 
   // Entry and exit functions for states (optional)
   virtual void entry(void) {}
@@ -27,3 +28,7 @@ class StateMachine : public tinyfsm::Fsm<StateMachine> {
 };
 
 using stateMachine = tinyfsm::FsmList<StateMachine>;
+
+template <typename E> void sendFSMEvent(E const &event) {
+  stateMachine::template dispatch<E>(event);
+}
