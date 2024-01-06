e90805aa288713ef5789b919133a6aec3966b5f3
Josh Jenkins <clearharry@gmail.com>
2024-01-06 16:29:44 +1000

    - Remove unnecessary `#include "Logger.h"`. - Change `notifyImpl` to use a sentinel value to differentiate between two `onNotify` methods.

--- lib/System/EventManager/EventManager.h
+++ lib/System/EventManager/EventManager.h
@@ -4,6 +4,7 @@
 #include "Event.h"
 #include "IEventListener.h"
 #include <algorithm>
+#include <limits>
 #include <memory>
 #include <unordered_map>
 #include <vector>
@@ -33,17 +34,29 @@ public:
 
   // Overloaded notify method for events that don't require a value
   void notify(Event event, Parameter parameter) {
-    notifyImpl(event, parameter, 0.0f); // Using a default value
+    notifyImpl(event, parameter);
   }
 
 private:
   std::unordered_map<Event, std::vector<std::shared_ptr<IEventListener>>>
       _listeners;
 
-  void notifyImpl(Event event, Parameter parameter, float value) {
+  // Sentinent value used as a unique float value to check which onNotify method
+  // to use
+  static constexpr float SENTINEL_VALUE =
+      std::numeric_limits<float>::infinity();
+
+  void notifyImpl(Event event, Parameter parameter,
+                  float value = SENTINEL_VALUE) {
     if (_listeners.count(event) > 0) {
       for (auto &listener : _listeners[event]) {
-        listener->onNotify(event, parameter, value);
+        if (value == SENTINEL_VALUE) {
+          // Call the version without value
+          listener->onNotify(event, parameter);
+        } else {
+          // Call the version with value
+          listener->onNotify(event, parameter, value);
+        }
       }
     }
   }
