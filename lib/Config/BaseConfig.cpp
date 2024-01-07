40c5dfc4c5562afcb7a4168e374f360f54e7d24c
Josh Jenkins <clearharry@gmail.com>
2024-01-07 10:00:01 +1000

    constructor takes filename improved writing json to file

--- lib/Config/BaseConfig.cpp
+++ lib/Config/BaseConfig.cpp
@@ -1,15 +1,19 @@
 // BaseConfig.cpp
-
 #include "BaseConfig.h"
 #include "Error.h"
 #include "FileGuard.h"
 #include "JSONDeserializer.h"
 
-BaseConfig::BaseConfig(std::shared_ptr<IFileHandler> fileHandler)
-    : _fileHandler(fileHandler) {}
+BaseConfig::BaseConfig(std::shared_ptr<IFileHandler> fileHandler,
+                       const std::string &filename)
+    : _fileHandler(fileHandler), _filename(filename) {
+  if (!fileHandler) {
+    Error(Error::FileHandlerIsNull);
+  }
+}
 
-Error BaseConfig::load(const std::string &filename) {
-  FileGuard fileGuard(_fileHandler.get(), filename);
+Error BaseConfig::load() {
+  FileGuard fileGuard(_fileHandler.get(), _filename, "r");
   if (!fileGuard.isOpen()) {
     return Error(Error::FileOpenFailure);
   }
@@ -29,19 +33,40 @@ Error BaseConfig::load(const std::string &filename) {
 }
 
 Error BaseConfig::writeJsonToFile(const DynamicJsonDocument &doc,
-                                  const std::string &filename) const {
-  std::string output;
-  serializeJson(doc, output);
+                                  const std::string &filenameParameter) const {
+  if (!_fileHandler) {
+    return Error(Error::FileHandlerIsNull);
+  }
 
-  if (!_fileHandler->open(filename, "w")) {
+  constexpr size_t MaxStringSize = 4000; // Maximum string size for ESP32
+
+  if (measureJsonPretty(doc) > MaxStringSize) {
+    return Error(Error::JsonDocSizeExceededMaximum4000BytesForESP32);
+  }
+
+  // Determine the filename to use. The class _filename from the constructor
+  // will be used otherwise
+  std::string filename =
+      filenameParameter.empty() ? _filename : filenameParameter;
+
+  // Serialize the JSON document to a string
+  std::string output;
+  serializeJsonPretty(doc, output);
+
+  // Create a FileGuard for the filename in write mode
+  FileGuard fileGuard(_fileHandler.get(), filename, "w");
+
+  if (!fileGuard.isOpen()) {
     return Error(Error::FileWriteFailure);
   }
 
   if (!_fileHandler->write(output)) {
-    _fileHandler->close();
+    // FileGuard will automatically close the file when it goes out of scope
     return Error(Error::FileWriteFailure);
   }
 
-  _fileHandler->close();
+  Logger::info("Successfully saved: " + filename);
+
+  // FileGuard will automatically close the file when it goes out of scope
   return Error(Error::OK);
 }
