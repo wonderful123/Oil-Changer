9b1bbeaeb7ae511745ff5d6b549b50aed5216b4b
Josh Jenkins <clearharry@gmail.com>
2024-01-07 14:00:22 +1000

    settings are passed by value no smart pointer

--- lib/Config/MotorSettingsConfig.cpp
+++ lib/Config/MotorSettingsConfig.cpp
@@ -2,12 +2,10 @@
 
 #include "MotorSettingsConfig.h"
 #include "Error.h"
-#include "MotorSettings.h"
 
 MotorSettingsConfig::MotorSettingsConfig(
     std::shared_ptr<IFileHandler> fileHandler, const std::string &filename)
-    : BaseConfig(fileHandler, filename),
-      _settings(std::make_shared<MotorSettings>()){};
+    : BaseConfig(fileHandler, filename){};
 
 Error MotorSettingsConfig::parseJson(const DynamicJsonDocument &doc) {
   const JsonObjectConst motorSettings = doc["motorSettings"];
@@ -22,7 +20,8 @@ Error MotorSettingsConfig::parseJson(const DynamicJsonDocument &doc) {
   settings.minSpeed = motorSettings["minSpeed"].as<float>();
   settings.rampingEnabled = motorSettings["rampingEnabled"].as<bool>();
   settings.rampingOnDurationMs = motorSettings["rampingOnDurationMs"].as<int>();
-  settings.rampingOffDurationMs = motorSettings["rampingOffDurationMs"].as<int>();
+  settings.rampingOffDurationMs =
+      motorSettings["rampingOffDurationMs"].as<int>();
 
   Error err = motorSettingsValidation(settings);
 
@@ -30,7 +29,7 @@ Error MotorSettingsConfig::parseJson(const DynamicJsonDocument &doc) {
     return err;
   }
 
-  _settings = std::make_shared<MotorSettings>(settings);
+  _settings = settings;
   return Error::OK;
 }
 
@@ -56,25 +55,18 @@ Error MotorSettingsConfig::motorSettingsValidation(
 }
 
 Error MotorSettingsConfig::save() const {
-  // Check if the settings are initialized
-  if (!_settings) {
-    return Error(Error::MotorSettingsNotInitialized);
-  }
-
   // Create a JSON object and populate it with motor settings
   DynamicJsonDocument doc(1024);
   JsonObject jsonSettings = doc.createNestedObject("motorSettings");
 
-  jsonSettings["description"] = _settings->description;
-  jsonSettings["maxSpeed"] = _settings->maxSpeed;
-  jsonSettings["minSpeed"] = _settings->minSpeed;
-  jsonSettings["rampingEnabled"] = _settings->rampingEnabled;
-  jsonSettings["rampingOnDurationMs"] = _settings->rampingOnDurationMs;
-  jsonSettings["rampingOffDurationMs"] = _settings->rampingOffDurationMs;
+  jsonSettings["description"] = _settings.description;
+  jsonSettings["maxSpeed"] = _settings.maxSpeed;
+  jsonSettings["minSpeed"] = _settings.minSpeed;
+  jsonSettings["rampingEnabled"] = _settings.rampingEnabled;
+  jsonSettings["rampingOnDurationMs"] = _settings.rampingOnDurationMs;
+  jsonSettings["rampingOffDurationMs"] = _settings.rampingOffDurationMs;
 
   return writeJsonToFile(jsonSettings);
 }
 
-std::shared_ptr<MotorSettings> MotorSettingsConfig::getSettings() {
-  return _settings;
-}
+MotorSettings MotorSettingsConfig::getSettings() { return _settings; }
--- lib/Config/MotorSettingsConfig.h
+++ lib/Config/MotorSettingsConfig.h
@@ -2,22 +2,22 @@
 #pragma once
 
 #include "BaseConfig.h"
+#include "MotorSettings.h"
 
 class Error;
-class MotorSettings;
 
 class MotorSettingsConfig : public BaseConfig {
 public:
-  explicit MotorSettingsConfig(std::shared_ptr<IFileHandler> fileHandler,
-                               const std::string &filename);
+  MotorSettingsConfig(std::shared_ptr<IFileHandler> fileHandler,
+                      const std::string &filename);
   ~MotorSettingsConfig() override = default;
 
-  std::shared_ptr<MotorSettings> getSettings();
+  MotorSettings getSettings();
 
   Error save() const override;
 
 private:
-  std::shared_ptr<MotorSettings> _settings;
+  MotorSettings _settings;
   Error parseJson(const DynamicJsonDocument &doc) override;
   Error motorSettingsValidation(const MotorSettings &settings) const;
 };
