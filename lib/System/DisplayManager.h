7def7fe4976028d622d5cb6c2b14e868b88bd2f0
Josh Jenkins <clearharry@gmail.com>
2024-01-07 14:02:33 +1000

    initial code

--- lib/System/DisplayManager.h
+++ lib/System/DisplayManager.h
@@ -1,6 +1,17 @@
-// DisplayManager.h
-#pragma once
+#include "EventManager/EventManager.h"
+#include "IDisplayAdapter.h"
+#include <memory>
 
-class DisplayManager {
+class DisplayManager : public IEventListener,
+                       public std::enable_shared_from_this<DisplayManager> {
+public:
+  DisplayManager(std::shared_ptr<EventManager> eventManager,
+                 std::shared_ptr<IDisplayAdapter> displayAdapter);
+  ~DisplayManager();
 
+  void onNotify(Event type, Parameter parameter, float value) override;
+
+private:
+  std::shared_ptr<EventManager> _eventManager;
+  std::shared_ptr<IDisplayAdapter> _displayAdapter;
 };
