137555f032b975a245d0858935a8b3ba3c0bb90a
Josh Jenkins <clearharry@gmail.com>
2024-01-07 10:02:13 +1000

    add errors for motor settings

--- lib/Utils/Error.h
+++ lib/Utils/Error.h
@@ -6,65 +6,77 @@
 #include <string>
 
 // Macro list of error codes and their messages
-#define ERROR_CODE_DEFINITIONS                                               \
-  ERROR_CODE(OK, "No Error.")                                                \
-  ERROR_CODE(FileSystemMountFailure, "Failed to mount the file system.")     \
-  ERROR_CODE(FileOpenFailure, "Unable to open the specified file.")          \
-  ERROR_CODE(FileReadFailure, "Error encountered while reading the file.")   \
-  ERROR_CODE(FileSystemMountError, "Failed to mount the file system.")       \
-  ERROR_CODE(FileWriteFailure, "Error encountered while writing the file.")  \
-  ERROR_CODE(HardwareConfigComponentsKeyMissing,                             \
-             "Missing 'components' key in hardware configuration.")          \
-  ERROR_CODE(HardwareConfigInitError,                                        \
-             "Failed to initialize the hardware configuration.")             \
-  ERROR_CODE(HardwareConfigBuzzerInitError,                                  \
-             "Failed to initialize the buzzer.")                             \
-  ERROR_CODE(JsonInputInvalid,                                               \
-             "The provided JSON input is invalid or malformed.")             \
-  ERROR_CODE(JsonMemoryError,                                                \
-             "Insufficient memory to process the JSON document.")            \
-  ERROR_CODE(JsonInputEmpty, "The JSON input provided is empty.")            \
-  ERROR_CODE(JsonInputIncomplete,                                            \
-             "The JSON input is incomplete or ends abruptly.")               \
-  ERROR_CODE(JsonDeserializationFailure,                                     \
-             "Failed to deserialize the JSON document due to an error.")     \
-  ERROR_CODE(                                                                \
-      ConfigTypeNotRecognized,                                               \
-      "The specified configuration type is not recognized or supported.")    \
-  ERROR_CODE(DigitalIOModeOptionMissingOrInvalid,                            \
-             "Missing or invalid mode in DigitalIO options")                 \
-  ERROR_CODE(ADCInvalidAttenuationValue, "Invalid ADC attenuation value.")   \
-  ERROR_CODE(FlowMeterInitErrorNoPCNTUnitSpecified,                          \
-             "FlowMeter: No PCNT unit specified for GPIO pin")               \
-  ERROR_CODE(InteractionSettingsButtonInteractionKeyMissing,                 \
-             "Missing 'buttonInteraction' key in interactionSettings.json")  \
-  ERROR_CODE(InteractionSettingsCommonSettingsKeyMissing,                    \
-             "Missing 'common' key in interactionSettings.json")             \
-  ERROR_CODE(InteractionSettingsButtonsSubkeyMissing,                        \
-             "Missing 'buttons' subkey in interactionsSettings.json")        \
-  ERROR_CODE(InteractionSettingsBeepSettingsSubkeyMissing,                   \
-             "Missing 'beep' subkey in interactionSettings.json")            \
-  ERROR_CODE(InteractionSettingsFeedbackSubkeyMissing,                       \
-             "Missing 'feedback' subkey in interactionSettings.json")        \
-  ERROR_CODE(SerialHardwareConfigError,                                      \
-             "Failed to initialize the serial hardware configuration.")      \
-  ERROR_CODE(SerialInvalidUartPortNumber,                                    \
-             "ESP32 Serial: Invalid UART port number for ESP32.")            \
-  ERROR_CODE(HardwareComponentCreationFailure,                               \
-             "Failed to create a hardware component.")                       \
-  ERROR_CODE(BuzzerManagerInitializationFailure,                             \
-             "Failed to initialize BuzzerManager.")                          \
-  ERROR_CODE(ConfigManagerInteractionSettingsError,                          \
-             "Failed to return interaction settings from config manager.")   \
-  ERROR_CODE(ConfigManagerHardwareConfigNotFound,                            \
-             "Failed to return hardware configuration from config manager.") \
-  ERROR_CODE(HardwareConfigButtonsNotFound,                                  \
-             "No buttons found in hardware configuration.")                  \
-  ERROR_CODE(FileHandlerIsNull, "FileHandler is null.")                      \
+#define ERROR_CODE_DEFINITIONS                                                 \
+  ERROR_CODE(OK, "No Error.")                                                  \
+  ERROR_CODE(FileSystemMountFailure, "Failed to mount the file system.")       \
+  ERROR_CODE(FileOpenFailure, "Unable to open the specified file.")            \
+  ERROR_CODE(FileReadFailure, "Error encountered while reading the file.")     \
+  ERROR_CODE(FileSystemMountError, "Failed to mount the file system.")         \
+  ERROR_CODE(FileWriteFailure, "Error encountered while writing the file.")    \
+  ERROR_CODE(HardwareConfigComponentsKeyMissing,                               \
+             "Missing 'components' key in hardware configuration.")            \
+  ERROR_CODE(HardwareConfigInitError,                                          \
+             "Failed to initialize the hardware configuration.")               \
+  ERROR_CODE(HardwareConfigBuzzerInitError,                                    \
+             "Failed to initialize the buzzer.")                               \
+  ERROR_CODE(JsonInputInvalid,                                                 \
+             "The provided JSON input is invalid or malformed.")               \
+  ERROR_CODE(JsonMemoryError,                                                  \
+             "Insufficient memory to process the JSON document.")              \
+  ERROR_CODE(JsonInputEmpty, "The JSON input provided is empty.")              \
+  ERROR_CODE(JsonInputIncomplete,                                              \
+             "The JSON input is incomplete or ends abruptly.")                 \
+  ERROR_CODE(JsonDeserializationFailure,                                       \
+             "Failed to deserialize the JSON document due to an error.")       \
+  ERROR_CODE(                                                                  \
+      ConfigTypeNotRecognized,                                                 \
+      "The specified configuration type is not recognized or supported.")      \
+  ERROR_CODE(DigitalIOModeOptionMissingOrInvalid,                              \
+             "Missing or invalid mode in DigitalIO options")                   \
+  ERROR_CODE(ADCInvalidAttenuationValue, "Invalid ADC attenuation value.")     \
+  ERROR_CODE(FlowMeterInitErrorNoPCNTUnitSpecified,                            \
+             "FlowMeter: No PCNT unit specified for GPIO pin")                 \
+  ERROR_CODE(InteractionSettingsButtonInteractionKeyMissing,                   \
+             "Missing 'buttonInteraction' key in interactionSettings.json")    \
+  ERROR_CODE(InteractionSettingsCommonSettingsKeyMissing,                      \
+             "Missing 'common' key in interactionSettings.json")               \
+  ERROR_CODE(InteractionSettingsButtonsSubkeyMissing,                          \
+             "Missing 'buttons' subkey in interactionsSettings.json")          \
+  ERROR_CODE(InteractionSettingsBeepSettingsSubkeyMissing,                     \
+             "Missing 'beep' subkey in interactionSettings.json")              \
+  ERROR_CODE(InteractionSettingsFeedbackSubkeyMissing,                         \
+             "Missing 'feedback' subkey in interactionSettings.json")          \
+  ERROR_CODE(SerialHardwareConfigError,                                        \
+             "Failed to initialize the serial hardware configuration.")        \
+  ERROR_CODE(SerialInvalidUartPortNumber,                                      \
+             "ESP32 Serial: Invalid UART port number for ESP32.")              \
+  ERROR_CODE(HardwareComponentCreationFailure,                                 \
+             "Failed to create a hardware component.")                         \
+  ERROR_CODE(BuzzerManagerInitializationFailure,                               \
+             "Failed to initialize BuzzerManager.")                            \
+  ERROR_CODE(ConfigManagerInteractionSettingsError,                            \
+             "Failed to return interaction settings from config manager.")     \
+  ERROR_CODE(ConfigManagerHardwareConfigNotFound,                              \
+             "Failed to return hardware configuration from config manager.")   \
+  ERROR_CODE(HardwareConfigButtonsNotFound,                                    \
+             "No buttons found in hardware configuration.")                    \
+  ERROR_CODE(FileHandlerIsNull, "FileHandler is null.")                        \
+  ERROR_CODE(MotorSettingsJsonMissingMotorSettingsKey,                         \
+             "MotorSettings: Missing 'motorSettings' key")                     \
+  ERROR_CODE(MotorSettingsValidationMaxSpeedLessThanMinSpeed,                  \
+             "MotorSettings: Max speed must be greater than min speed.")       \
+  ERROR_CODE(MotorSettingsValidationMinSpeedLessThanOrEqualToZero,             \
+             "MotorSettings: Min speed must be greater than or equal to zero.")\
+  ERROR_CODE(MotorSettingsValidationRampingDurationLessThanZero,               \
+             "MotorSettings: Ramping duration must be greater than zero.")     \
+  ERROR_CODE(MotorSettingsNotInitialized,                                      \
+             "MotorSettings not initialized.")                                 \
+  ERROR_CODE(JsonDocSizeExceededMaximum4000BytesForESP32,                      \
+             "JSON document size exceeded maximum of 4000 bytes for ESP32.")   \
 
 #undef ERROR_CODE
 
-class Error {
+    class Error {
 public:
   enum Code {
 #define ERROR_CODE(code, message) code,
