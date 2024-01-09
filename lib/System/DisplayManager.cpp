cb828ba46e830cd618ab617c2373162ae3a2cee9
Josh Jenkins <clearharry@gmail.com>
2024-01-09 18:23:23 +1000

    completed implementation

--- lib/System/DisplayManager.cpp
+++ lib/System/DisplayManager.cpp
@@ -1,3 +1,87 @@
 // DisplayManager.cpp
-
 #include "DisplayManager.h"
+#include "Logger.h"
+#include <iomanip> // for std::setprecision
+#include <sstream>
+
+DisplayManager::DisplayManager(
+    std::shared_ptr<EventManager> eventManager,
+    std::shared_ptr<OilChangeTracker> oilChangeTracker,
+    std::map<std::string, std::shared_ptr<IDisplay>> &displays)
+    : _eventManager(eventManager), _oilChangeTracker(oilChangeTracker),
+      _displays(displays) {
+
+  auto primaryIt = _displays.find("Display1");
+  if (primaryIt != _displays.end()) {
+    _primaryDisplay = primaryIt->second;
+  } else {
+    Logger::info("[DisplayManager] Primary display not found");
+  }
+  // Only attempt to set the secondary display if 2 displays are given
+  if (displays.size() > 1) {
+    auto secondaryIt = _displays.find("Display2");
+    if (secondaryIt != _displays.end()) {
+      _secondaryDisplay = secondaryIt->second;
+    } else {
+      Logger::info("[DisplayManager] Secondary display not found");
+    }
+  }
+  // If secondaryDisplayId is empty, _secondaryDisplay remains uninitialized
+  // (nullptr)
+}
+
+DisplayManager::~DisplayManager() {
+  _eventManager->unsubscribe(shared_from_this(), Event::Display);
+}
+
+void DisplayManager::initialize() {
+  _eventManager->subscribe(shared_from_this(), Event::Display);
+  _lastUpdate = std::chrono::steady_clock::now();
+}
+
+void DisplayManager::onNotify(Event type, Parameter parameter,
+                              const std::string &message) {
+  if (type == Event::Display) {
+    if (parameter == Parameter::DisplayMessage && _primaryDisplay) {
+      _primaryDisplay->displayMessage(message);
+    } else if (parameter == Parameter::SecondaryDisplayMessage &&
+               _secondaryDisplay) {
+      _secondaryDisplay->displayMessage(message);
+    }
+  }
+}
+
+void DisplayManager::update() {
+  auto now = std::chrono::steady_clock::now();
+  if (now - _lastUpdate >=
+      std::chrono::milliseconds(500)) { // Update every 500ms
+    auto currentData = _oilChangeTracker->getCurrentData();
+    std::string message = formatDisplayMessage(currentData);
+    // Update displays
+    if (_primaryDisplay) {
+      _primaryDisplay->displayMessage(message);
+    }
+    _lastData = currentData;
+    _lastUpdate = now;
+  }
+}
+
+std::string DisplayManager::formatDisplayMessage(const OilChangeTracker::TrackerData &data) {
+  std::stringstream ss;
+  ss << std::fixed
+     << std::setprecision(2); // Set fixed floating point with 2 decimal places
+  ss << "FillCap:" << data.fillCapacity << ";"
+     << "AmtFilled:" << data.amountFilled << ";"
+     << "AmtExtracted:" << data.amountExtracted << ";"
+     << "FillRate:" << data.fillFlowRate << ";"
+     << "ExtractRate:" << data.extractFlowRate << ";"
+     << "Voltage:" << data.voltage << ";"
+     << "Temp:" << data.oilTemperature << ";"
+     << "FillLPS:" << convertLPSState(data.fillLPSState) << ";"
+     << "ExtractLPS:" << convertLPSState(data.extractLPSState);
+  return ss.str();
+}
+
+std::string DisplayManager::convertLPSState(int state) {
+  return state == 0 ? "low" : "high";
+}
--- lib/System/DisplayManager.h
+++ lib/System/DisplayManager.h
@@ -1,17 +1,42 @@
+// DisplayManager.h
+#pragma once
+
 #include "EventManager/EventManager.h"
-#include "IDisplayAdapter.h"
+#include "IDisplay.h"
+#include "OilChangeTracker.h"
+#include <chrono>
+#include <map>
 #include <memory>
 
 class DisplayManager : public IEventListener,
                        public std::enable_shared_from_this<DisplayManager> {
 public:
   DisplayManager(std::shared_ptr<EventManager> eventManager,
-                 std::shared_ptr<IDisplayAdapter> displayAdapter);
+                 std::shared_ptr<OilChangeTracker> oilChangeTracker,
+                 std::map<std::string, std::shared_ptr<IDisplay>> &displays);
   ~DisplayManager();
 
-  void onNotify(Event type, Parameter parameter, float value) override;
+  void initialize();
+
+  void onNotify(Event type, Parameter parameter,
+                const std::string &message) override;
+
+  void update();
 
 private:
   std::shared_ptr<EventManager> _eventManager;
-  std::shared_ptr<IDisplayAdapter> _displayAdapter;
+  std::shared_ptr<OilChangeTracker> _oilChangeTracker;
+  std::shared_ptr<IDisplay> _primaryDisplay;
+  std::shared_ptr<IDisplay> _secondaryDisplay;
+  std::map<std::string, std::shared_ptr<IDisplay>> _displays;
+
+  OilChangeTracker::TrackerData _lastData;
+
+  // Throttling related variables
+  std::chrono::steady_clock::time_point _lastUpdate;
+  const std::chrono::milliseconds _updateInterval{
+      500}; // Update interval in milliseconds
+
+  std::string formatDisplayMessage(const OilChangeTracker::TrackerData &data);
+  std::string convertLPSState(int state);
 };
