c43f3d6aaa97cf5f2a3a276f460608da6354bb53
Josh Jenkins <clearharry@gmail.com>
2024-01-09 18:25:27 +1000

    move initialization to system factory add new component periodic updates

--- lib/System/SystemController.cpp
+++ lib/System/SystemController.cpp
@@ -2,156 +2,30 @@
 
 #include "SystemController.h"
 
-// External dependencies
 #include <memory>
 
 // Project-specific headers
 #include "ButtonController.h"
-#include "BuzzerManager.h"
-#include "ConfigManager.h"
-#include "ConfigTypes.h"
-#include "HardwareFactory.h"
-#include "HardwareManager.h"
-#include "IButton.h"
-#include "IBuzzer.h"
-#include "InteractionSettings.h"
-#include "InteractionSettingsConfig.h"
+#include "DisplayManager.h"
 #include "Logger.h"
 #include "MotorController.h"
-#include "MotorSettingsConfig.h"
+#include "SensorManager.h"
 
 SystemController::SystemController(
-    std::shared_ptr<ConfigManager> configManager,
-    std::shared_ptr<HardwareManager> hardwareManager,
-    std::shared_ptr<EventManager> eventManager)
-    : _configManager(configManager), _hardwareManager(hardwareManager),
-      _eventManager(eventManager) {}
-
-void SystemController::initializeSystemComponents() {
-  if (!loadInteractionSettings()) {
-    Logger::error("[SystemController] Failed to load interaction settings");
-    return;
-  }
-
-  Logger::info("[SystemController] Creating ButtonController");
-  if (initializeButtonController(_interactionSettings) != Error::OK) {
-    Logger::error("[SystemController] Failed to initialize Button Controller");
-    return;
-  }
-
-  Logger::info("[SystemController] Creating BuzzerManager");
-  if (initializeBuzzerManager(_interactionSettings, _eventManager) !=
-      Error::OK) {
-    Logger::error("[SystemController] Failed to initialize Buzzer Manager");
-    return;
-  }
-
-  Logger::info("[SystemController] Creating MotorController");
-  if (initializeMotorController(_eventManager) != Error::OK) {
-    Logger::error("[SystemController] Failed to initialize Motor Controller");
-    return;
-  }
-
-  Logger::info("[SystemController] System components initialized");
-}
-
-bool SystemController::loadInteractionSettings() {
-  auto interactionSettingsConfig =
-      _configManager->getConfig<InteractionSettingsConfig>(
-          ConfigType::INTERACTION_SETTINGS);
-
-  if (!interactionSettingsConfig) {
-    Logger::error("[SystemController] Interaction settings config not found");
-    return false;
-  }
-
-  _interactionSettings = interactionSettingsConfig->getSettings();
-  return true;
-}
-
-Error SystemController::initializeButtonController(
-    std::shared_ptr<InteractionSettings> &interactionSettings) {
-  _buttonController = std::make_shared<ButtonController>(interactionSettings);
-
-  // Check and log if button controller is not created
-  if (!_buttonController) {
-    Logger::error("[SystemController] Button controller not created");
-    return Error::HardwareConfigButtonsNotFound;
-  }
-
-  auto buttonComponents =
-      _hardwareManager->getComponentsByType<IButton>("Button");
-  if (buttonComponents.empty()) {
-    Logger::error(
-        "[SystemController] No buttons found in hardware configuration");
-    return Error::HardwareConfigButtonsNotFound;
-  }
-
-  // Register buttons
-  for (auto button : buttonComponents) {
-    _buttonController->registerButton(button);
-  }
-
-  return Error::OK;
-}
-
-Error SystemController::initializeBuzzerManager(
-    std::shared_ptr<InteractionSettings> &interactionSettings,
-    std::shared_ptr<EventManager> eventManager) {
-  if (!eventManager)
-    Logger::info("FUCKER");
-  auto buzzer = _hardwareManager->getComponentById<IBuzzer>("Buzzer");
-  if (!buzzer) {
-    return Error::HardwareConfigBuzzerInitError;
-  }
-
-  _buzzerManager = std::make_shared<BuzzerManager>(buzzer, interactionSettings,
-                                                   eventManager);
-  _buzzerManager->initialize();
-
-  return Error::OK;
-}
-
-Error SystemController::initializeMotorController(
-    std::shared_ptr<EventManager> eventManager) {
-  // Get settings config for motor controller
-  auto motorSettingsConfig =
-      _configManager->getConfig<MotorSettingsConfig>(ConfigType::MOTOR);
-  if (!motorSettingsConfig) {
-    Logger::error("[SystemController] Motor settings config not found");
-    return Error(Error::MotorSettingsConfigNotLoaded);
-  }
-
-  auto motorSettings = motorSettingsConfig->getSettings();
-
-  auto speedControlDAC = _hardwareManager->getComponentById<IDAC>("MotorSpeed");
-  auto motorControlFill =
-      _hardwareManager->getComponentById<IDigitalIO>("MotorControlFill");
-  auto motorControlExtract =
-      _hardwareManager->getComponentById<IDigitalIO>("MotorControlExtract");
-
-  _motorController =
-      std::make_shared<MotorController>(eventManager, motorSettings);
-  _motorController->initialize(speedControlDAC, motorControlFill,
-                               motorControlExtract);
-
-  return Error::OK;
-}
+    std::shared_ptr<ButtonController> buttonController,
+    std::shared_ptr<MotorController> motorController,
+    std::shared_ptr<SensorManager> sensorManager,
+    std::shared_ptr<DisplayManager> displayManager)
+    : _buttonController(buttonController), _motorController(motorController),
+      _sensorManager(sensorManager), _displayManager(displayManager) {}
 
 void SystemController::performPeriodicUpdate() {
-  if (_buttonController) {
+  if (_buttonController)
     _buttonController->processButtonStates();
-  }
-
-  if (_motorController) {
+  if (_motorController)
     _motorController->update(); // For motor speed ramping
-  }
-}
-
-std::shared_ptr<ButtonController> SystemController::getButtonController() {
-  return _buttonController;
-}
-
-std::shared_ptr<BuzzerManager> SystemController::getBuzzerManager() {
-  return _buzzerManager;
+  if (_sensorManager)
+    _sensorManager->update();
+  if (_displayManager)
+    _displayManager->update();
 }
--- lib/System/SystemController.h
+++ lib/System/SystemController.h
@@ -1,46 +1,25 @@
 // SystemController.h
 #pragma once
 
-#include "Error.h"
 #include <memory>
 
-class StateMachine;
 class ButtonController;
-class HardwareManager;
-class BuzzerManager;
-class ConfigManager;
-class InteractionSettings;
 class MotorController;
-class EventManager;
+class SensorManager;
+class DisplayManager;
 
 class SystemController {
 public:
-  SystemController(std::shared_ptr<ConfigManager> configManager,
-                   std::shared_ptr<HardwareManager> hardwareManager,
-                   std::shared_ptr<EventManager> eventManager);
-
-  void initializeSystemComponents();
-  bool loadInteractionSettings();
+  SystemController(std::shared_ptr<ButtonController> buttonController,
+                   std::shared_ptr<MotorController> motorController,
+                   std::shared_ptr<SensorManager> sensorManager,
+                   std::shared_ptr<DisplayManager> displayManager);
 
   void performPeriodicUpdate();
 
-  std::shared_ptr<ButtonController> getButtonController();
-  std::shared_ptr<BuzzerManager> getBuzzerManager();
-
 private:
-  std::shared_ptr<ConfigManager> _configManager;
-  std::shared_ptr<StateMachine> _stateMachine;
-  std::shared_ptr<HardwareManager> _hardwareManager;
   std::shared_ptr<ButtonController> _buttonController;
-  std::shared_ptr<BuzzerManager> _buzzerManager;
   std::shared_ptr<MotorController> _motorController;
-  std::shared_ptr<EventManager> _eventManager;
-  std::shared_ptr<InteractionSettings> _interactionSettings;
-
-  Error initializeButtonController(
-      std::shared_ptr<InteractionSettings> &interactionSettings);
-  Error initializeBuzzerManager(
-      std::shared_ptr<InteractionSettings> &interactionSettings,
-      std::shared_ptr<EventManager> eventManager);
-  Error initializeMotorController(std::shared_ptr<EventManager> eventManager);
+  std::shared_ptr<SensorManager> _sensorManager;
+  std::shared_ptr<DisplayManager> _displayManager;
 };
