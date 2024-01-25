4786c02d2361fa6708c52a66c570e51bb33bbda7
Josh Jenkins <clearharry@gmail.com>
2024-01-25 10:27:49 +1000

    fix inheritance - should be DisplayBase

--- lib/Hardware/ESP32/ESP32SerialDisplay.cpp
+++ lib/Hardware/ESP32/ESP32SerialDisplay.cpp
@@ -1,15 +1,16 @@
 // ESP32SerialDisplay.cpp
 #include "ESP32SerialDisplay.h"
 #include "HardwarePinConfig.h"
+#include "Logger.h"
 #include <Arduino.h>
 
 ESP32SerialDisplay::ESP32SerialDisplay(std::shared_ptr<ISerial> serial,
                                        const std::string &displayId)
-    : _serial(serial), _displayId(displayId) {
+    : _serial(serial), DisplayBase(displayId) {
   // Ensure the serial interface is valid before attempting to begin
   // communication.
   if (!_serial) {
-    throw std::invalid_argument("Serial interface is required");
+    Logger::error("[ESP32SerialDisplay] Serial interface is required");
   }
   _serial->begin();
 }
@@ -21,9 +22,7 @@ ESP32SerialDisplay::~ESP32SerialDisplay() {
 }
 
 void ESP32SerialDisplay::displayMessage(const std::string &data) {
-  std::string formattedMessage = "<" + _displayId + ";" + data + ">";
+  std::string formattedMessage = "<" + _id + ";" + data + ">";
   _serial->write(
       formattedMessage); // Send message over UART via ISerial interface.
 }
-
-std::string ESP32SerialDisplay::getId() const { return _displayId; }
--- lib/Hardware/ESP32/ESP32SerialDisplay.h
+++ lib/Hardware/ESP32/ESP32SerialDisplay.h
@@ -1,12 +1,12 @@
 // ESP32SerialDisplay.h
 #pragma once
 
-#include "IDisplay.h"
+#include "DisplayBase.h"
 #include "ISerial.h"
 #include <memory>
 #include <string>
 
-class ESP32SerialDisplay : public IDisplay {
+class ESP32SerialDisplay : public DisplayBase {
 public:
   // Now the constructor accepts an ISerial object and initializes IDisplay with
   // the config.
@@ -14,9 +14,7 @@ public:
                      const std::string &displayId);
   virtual ~ESP32SerialDisplay();
   void displayMessage(const std::string &data) override;
-  std::string getId() const override;
 
 private:
   std::shared_ptr<ISerial> _serial; // Holds the serial interface.
-  std::string _displayId;           // Unique identifier for the display.
 };
